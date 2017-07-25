8158fec31270e33fe5e6367fe8def7107a9e820d
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-07-25 15:33:11 +0300

    IO: utils

--- integration/kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/Strings.kt
+++ integration/kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/Strings.kt
@@ -1,11 +1,14 @@
 package kotlinx.coroutines.experimental.io
 
-suspend fun ByteBufferChannel.readASCIILine(estimate: Int = 16): String? {
+suspend fun ByteReadChannel.readASCIILine(estimate: Int = 16, limit: Int = Int.MAX_VALUE): String? {
     val sb = StringBuilder(estimate)
-    return if (readUTF8LineTo(sb)) sb.toString() else null
+    return if (readUTF8LineTo(sb, limit)) sb.toString() else null
 }
 
-suspend fun ByteBufferChannel.readUTF8Line(estimate: Int = 16): String? {
+suspend fun ByteReadChannel.readUTF8Line(estimate: Int = 16, limit: Int = Int.MAX_VALUE): String? {
     val sb = StringBuilder(estimate)
-    return if (readUTF8LineTo(sb)) sb.toString() else null
+    return if (readUTF8LineTo(sb, limit)) sb.toString() else null
 }
+
+@Deprecated("Use readUTF8Line or readASCIILine instead", ReplaceWith("readUTF8Line(estimate, limit)"))
+suspend fun ByteReadChannel.readLine(estimate: Int = 16, limit: Int = Int.MAX_VALUE): String? = readUTF8Line(estimate, limit)
