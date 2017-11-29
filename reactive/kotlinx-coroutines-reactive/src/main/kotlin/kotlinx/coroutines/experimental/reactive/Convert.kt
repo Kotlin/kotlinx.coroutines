e19ee04ca72316f9d67a58d35b8be398b059f1f5
Francesco Vasco <fsco_v-github@yahoo.it>
2017-11-29 22:12:35 +0100

    Add DefaultDispatcher on some reactive operators

--- reactive/kotlinx-coroutines-reactive/src/main/kotlin/kotlinx/coroutines/experimental/reactive/Convert.kt
+++ reactive/kotlinx-coroutines-reactive/src/main/kotlin/kotlinx/coroutines/experimental/reactive/Convert.kt
@@ -16,6 +16,7 @@
 
 package kotlinx.coroutines.experimental.reactive
 
+import kotlinx.coroutines.experimental.DefaultDispatcher
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
 import org.reactivestreams.Publisher
 import kotlin.coroutines.experimental.CoroutineContext
@@ -28,7 +29,7 @@ import kotlin.coroutines.experimental.CoroutineContext
  *
  * @param context -- the coroutine context from which the resulting observable is going to be signalled
  */
-public fun <T> ReceiveChannel<T>.asPublisher(context: CoroutineContext): Publisher<T> = publish(context) {
+public fun <T> ReceiveChannel<T>.asPublisher(context: CoroutineContext = DefaultDispatcher): Publisher<T> = publish(context) {
     for (t in this@asPublisher)
         send(t)
 }
