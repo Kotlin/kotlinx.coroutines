1e402370935e59ba579f13d2a15b690643f390c0
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-12-05 16:16:50 +0300

    IO: update ByteReadChannel.cancel/close documentation

--- core/kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/ByteReadChannel.kt
+++ core/kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/ByteReadChannel.kt
@@ -145,6 +145,16 @@ public interface ByteReadChannel {
      */
     suspend fun read(min: Int = 1, block: (ByteBuffer) -> Unit)
 
+    /**
+     * Close channel with optional [cause] cancellation. Unlike [ByteWriteChannel.close] that could close channel
+     * normally, cancel does always close with error so any operations on this channel will always fail
+     * and all suspensions will be resumed with exception.
+     *
+     * Please note that if the channel has been provided by [reader] or [writer] then the corresponding owning
+     * coroutine will be cancelled as well
+     *
+     * @see ByteWriteChannel.close
+     */
     fun cancel(cause: Throwable? = null): Boolean
 }
 
