9d47b52e9c0717a336e53ff22f8e8002ca5c11b1
Roman Elizarov <elizarov@gmail.com>
2017-12-21 22:40:34 +0300

    MPP: Heap test for JS

--- js/kotlinx-coroutines-core-js/src/test/kotlin/kotlinx/coroutines/experimental/internal/HeapTest.kt
+++ js/kotlinx-coroutines-core-js/src/test/kotlin/kotlinx/coroutines/experimental/internal/HeapTest.kt
@@ -1,26 +1,10 @@
-/*
- * Copyright 2016-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package kotlinx.coroutines.experimental.internal
 
+import kotlin.js.Math
 import kotlin.test.*
-import java.util.*
 
-class ThreadSafeHeapTest {
-    class Node(val value: Int) : ThreadSafeHeapNode, Comparable<Node> {
+class HeapTest {
+    class Node(val value: Int) : HeapNode, Comparable<Node> {
         override var index = -1
         override fun compareTo(other: Node): Int = value.compareTo(other.value)
         override fun equals(other: Any?): Boolean = other is Node && other.value == value
@@ -30,7 +14,7 @@ class ThreadSafeHeapTest {
 
     @Test
     fun testBasic() {
-        val h = ThreadSafeHeap<Node>()
+        val h = Heap<Node>()
         assertEquals(null, h.peek())
         val n1 = Node(1)
         h.addLast(n1)
@@ -63,9 +47,8 @@ class ThreadSafeHeapTest {
     @Test
     fun testRandomSort() {
         val n = 1000
-        val r = Random(1)
-        val h = ThreadSafeHeap<Node>()
-        val a = IntArray(n) { r.nextInt() }
+        val h = Heap<Node>()
+        val a = IntArray(n) { (Math.random() * 10000).toInt() }
         repeat(n) { h.addLast(Node(a[it])) }
         a.sort()
         repeat(n) { assertEquals(Node(a[it]), h.removeFirstOrNull()) }
