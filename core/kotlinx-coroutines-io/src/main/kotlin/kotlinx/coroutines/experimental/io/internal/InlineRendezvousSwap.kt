0939f23f286b7f52ffe4d6f043f841981aa8eaf4
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-12-22 19:57:30 +0300

    Make mutable continuation internally accessible

--- core/kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/internal/InlineRendezvousSwap.kt
+++ core/kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/internal/InlineRendezvousSwap.kt
@@ -1,9 +1,6 @@
 package kotlinx.coroutines.experimental.io.internal
 
-import kotlinx.atomicfu.*
 import kotlinx.coroutines.experimental.*
-import java.util.concurrent.CancellationException
-import kotlin.coroutines.experimental.*
 import kotlin.coroutines.experimental.intrinsics.*
 
 /**
@@ -13,8 +10,8 @@ import kotlin.coroutines.experimental.intrinsics.*
  * - no poll/offer
  */
 internal class InlineRendezvousSwap<T : Any> {
-    private val senderCont = DelegatedContinuation<Unit>()
-    private val receiverCont = DelegatedContinuation<T>()
+    private val senderCont = MutableDelegateContinuation<Unit>()
+    private val receiverCont = MutableDelegateContinuation<T>()
 
     @Suppress("NOTHING_TO_INLINE")
     suspend inline fun send(e: T) = suspendCoroutineOrReturn<Unit> { c ->
@@ -42,74 +39,6 @@ internal class InlineRendezvousSwap<T : Any> {
         result
     }
 
-    internal class DelegatedContinuation<T : Any> : Continuation<T> {
-        private val delegate = atomic<Continuation<T>?>(null)
-        private var result: Any? = null
-        private var e: Throwable? = null
-
-        private fun reset() {
-            delegate.getAndSet(null)?.takeUnless { it === this }?.resumeWithException(Cancellation)
-            result = null
-            e = null
-        }
-
-        fun swap(actual: Continuation<T>): Any? {
-            require(actual !== this)
-
-            delegate.update { c ->
-                when {
-                    c === this -> return result().also { reset() }
-                    else -> actual
-                }
-            }
-
-            return COROUTINE_SUSPENDED
-        }
-
-        private fun result(): Any? {
-            e?.let { reset(); throw it }
-            return result
-        }
-
-        override val context: CoroutineContext
-            get() = delegate.value?.takeUnless { it === this }?.context ?: EmptyCoroutineContext
-
-        override fun resumeWithException(exception: Throwable) {
-            e = exception
-            val c = delegate.getAndUpdate { c ->
-                when {
-                    c === this -> return
-                    c == null -> this
-                    else -> null
-                }
-            }
-
-            if (c != null) {
-                reset()
-                c.resumeWithException(exception)
-            }
-        }
-
-        override fun resume(value: T) {
-            result = value
-            val c = delegate.getAndUpdate { c ->
-                when {
-                    c === this -> return
-                    c == null -> this
-                    else -> null
-                }
-            }
-
-            if (c != null) {
-                reset()
-                c.resume(value)
-            }
-        }
-    }
-
-    companion object {
-        private val Cancellation = CancellationException()
-    }
 }
 
 fun main(args: Array<String>) {
---
+++ core/kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/internal/MutableDelegateContinuation.kt
@@ -0,0 +1,146 @@
+package kotlinx.coroutines.experimental.io.internal
+
+import kotlinx.atomicfu.*
+import kotlinx.coroutines.experimental.*
+import kotlin.coroutines.experimental.*
+import kotlin.coroutines.experimental.intrinsics.*
+
+/**
+ * Semi-cancellable reusable continuation. Unlike regular continuation this implementation has limitations:
+ * - could be resumed only once per swap, undefined behaviour otherwise
+ * - [T] should be neither [Throwable] nor [Continuation]
+ * - value shouldn't be null
+ */
+internal class MutableDelegateContinuation<T : Any> : Continuation<T> {
+    private val state = atomic<Any?>(null)
+    private val handler = atomic<JobRelation?>(null)
+
+    fun swap(actual: Continuation<T>): Any {
+        loop@while (true) {
+            val before = state.value
+
+            when (before) {
+                null -> {
+                    if (!state.compareAndSet(null, actual)) continue@loop
+                    parent(actual.context)
+                    return COROUTINE_SUSPENDED
+                }
+                else -> {
+                    if (!state.compareAndSet(before, null)) continue@loop
+                    if (before is Throwable) throw before
+                    @Suppress("UNCHECKED_CAST")
+                    return before as T
+                }
+            }
+        }
+    }
+
+    fun close() {
+        resumeWithException(Cancellation)
+        handler.getAndSet(null)?.dispose()
+    }
+
+    private fun parent(context: CoroutineContext) {
+        val job = context[Job]
+        if (handler.value?.job === job) return
+
+        if (job == null) {
+            handler.getAndSet(null)?.dispose()
+        } else {
+            val handler = JobRelation(job)
+            val old = this.handler.getAndUpdate { j ->
+                when {
+                    j == null -> handler
+                    j.job === job -> return
+                    else -> handler
+                }
+            }
+            old?.dispose()
+        }
+    }
+
+    override val context: CoroutineContext
+        get() = (state.value as? Continuation<*>)?.context ?: EmptyCoroutineContext
+
+    override fun resume(value: T) {
+        loop@while(true) {
+            val before = state.value
+
+            when (before) {
+                null -> {
+                    if (!state.compareAndSet(null, value)) continue@loop
+                    return
+                }
+                is Continuation<*> -> {
+                    if (!state.compareAndSet(before, null)) continue@loop
+                    @Suppress("UNCHECKED_CAST")
+                    val cont = before as Continuation<T>
+                    return cont.resume(value)
+                }
+                else -> return
+            }
+        }
+    }
+
+    override fun resumeWithException(exception: Throwable) {
+        loop@while(true) {
+            val before = state.value
+
+            when (before) {
+                null -> {
+                    if (!state.compareAndSet(null, exception)) continue@loop
+                    return
+                }
+                is Continuation<*> -> {
+                    if (!state.compareAndSet(before, null)) continue@loop
+                    @Suppress("UNCHECKED_CAST")
+                    val cont = before as Continuation<T>
+                    return cont.resumeWithException(exception)
+                }
+                else -> return
+            }
+        }
+    }
+
+    private fun resumeWithExceptionContinuationOnly(job: Job, exception: Throwable) {
+        var c: Continuation<*>? = null
+
+        state.update {
+            if (it !is Continuation<*>) return
+            if (it.context[Job] !== job) return
+            c = it
+            null
+        }
+
+        c!!.resumeWithException(exception)
+    }
+
+    private inner class JobRelation(val job: Job) : CompletionHandler, DisposableHandle {
+        private var handler: DisposableHandle = NonDisposableHandle
+
+        init {
+            val h = job.invokeOnCompletion(onCancelling = true, handler = this)
+            if (job.isActive) {
+                handler = h
+            }
+        }
+
+        override fun invoke(cause: Throwable?) {
+            this@MutableDelegateContinuation.handler.compareAndSet(this, null)
+            dispose()
+
+            if (cause != null) {
+                resumeWithExceptionContinuationOnly(job, cause)
+            }
+        }
+
+        override fun dispose() {
+            handler.dispose()
+            handler = NonDisposableHandle
+        }
+    }
+
+    private companion object {
+        val Cancellation = CancellationException("Continuation terminated")
+    }
+}
