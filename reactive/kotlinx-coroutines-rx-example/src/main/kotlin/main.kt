727331c2aee0e47c5392e2b816b2efc8d1c13b6c
takahirom <takam.dev@gmail.com>
2018-03-04 14:02:32 +0900

    Update RxJava1 to RxJava2 in kotlinx-coroutines-rx-example

--- reactive/kotlinx-coroutines-rx-example/src/main/kotlin/main.kt
+++ reactive/kotlinx-coroutines-rx-example/src/main/kotlin/main.kt
@@ -14,26 +14,25 @@
  * limitations under the License.
  */
 
-
+import io.reactivex.Single
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.rx1.awaitSingle
+import kotlinx.coroutines.experimental.rx2.await
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
-import rx.Observable
 
 interface GitHub {
     @GET("/repos/{owner}/{repo}/contributors")
     fun contributors(
             @Path("owner") owner: String,
             @Path("repo") repo: String
-    ): Observable<List<Contributor>>
+    ): Single<List<Contributor>>
 
     @GET("users/{user}/repos")
-    fun listRepos(@Path("user") user: String): Observable<List<Repo>>
+    fun listRepos(@Path("user") user: String): Single<List<Repo>>
 }
 
 data class Contributor(val login: String, val contributions: Int)
@@ -43,7 +42,7 @@ fun main(args: Array<String>) {
     val retrofit = Retrofit.Builder().apply {
         baseUrl("https://api.github.com")
         addConverterFactory(GsonConverterFactory.create())
-        addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        addCallAdapterFactory(RxJava2CallAdapterFactory.create())
     }.build()
 
     val github = retrofit.create(GitHub::class.java)
@@ -51,13 +50,13 @@ fun main(args: Array<String>) {
     launch(CommonPool) {
         val contributors =
                 github.contributors("JetBrains", "Kotlin")
-                      .awaitSingle().take(10)
+                      .await().take(10)
 
         for ((name, contributions) in contributors) {
             println("$name has $contributions contributions, other repos: ")
 
             val otherRepos =
-                    github.listRepos(name).awaitSingle()
+                    github.listRepos(name).await()
                           .map(Repo::name).joinToString(", ")
 
             println(otherRepos)
