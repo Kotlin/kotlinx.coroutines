c2fba3a62b208f0148975b48e8b8bde33d41e30a
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2018-07-06 19:22:08 +0300

    Provide HandlerContext.immediate extension to immediately execute tasks which are invoked from the right context
    Fixes #381

--- ui/kotlinx-coroutines-android/src/HandlerContext.kt
+++ ui/kotlinx-coroutines-android/src/HandlerContext.kt
@@ -25,15 +25,40 @@ private const val MAX_DELAY = Long.MAX_VALUE / 2 // cannot delay for too long on
 
 /**
  * Implements [CoroutineDispatcher] on top of an arbitrary Android [Handler].
- * @param handler a handler.
- * @param name an optional name for debugging.
  */
-public class HandlerContext(
+public class HandlerContext private constructor(
     private val handler: Handler,
-    private val name: String? = null
+    private val name: String?,
+    private val invokeImmediately: Boolean
 ) : CoroutineDispatcher(), Delay {
+    /**
+     * Creates [CoroutineDispatcher] for the given Android [handler].
+     *
+     * @param handler a handler.
+     * @param name an optional name for debugging.
+     */
+    public constructor(
+        handler: Handler,
+        name: String? = null
+    ) : this(handler, name, false)
+
+    @Volatile
+    private var _immediate: HandlerContext? = if (invokeImmediately) this else null
+
+    /**
+     * Returns dispatcher that executes coroutines immediately when it is already in the right handler context
+     * (current looper is the same as [handler] looper). See [isDispatchNeeded] documentation on
+     * why this should not be done by default.
+     */
+    public val immediate: HandlerContext = _immediate ?:
+        HandlerContext(handler, name, true).also { _immediate = it }
+
     @Volatile
     private var _choreographer: Choreographer? = null
+
+    override fun isDispatchNeeded(context: CoroutineContext): Boolean {
+        return !invokeImmediately || Looper.myLooper() != handler.looper
+    }
 
     override fun dispatch(context: CoroutineContext, block: Runnable) {
         handler.post(block)
