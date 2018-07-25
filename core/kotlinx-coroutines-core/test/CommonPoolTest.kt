df800024e211dec2d390bec13f79ef4e75d12245
Roman Elizarov <elizarov@gmail.com>
2018-07-25 11:07:51 +0300

    Fixed package directive in CommonPoolTest

--- core/kotlinx-coroutines-core/test/CommonPoolTest.kt
+++ core/kotlinx-coroutines-core/test/CommonPoolTest.kt
@@ -2,7 +2,8 @@
  * Copyright 2016-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import kotlinx.coroutines.experimental.*
+package kotlinx.coroutines.experimental
+
 import org.junit.*
 import org.junit.Assert.*
 import java.lang.reflect.*
