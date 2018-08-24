efb8d1d3a81fb70601109f80eba45e11db9359de
Lucas <lucasvalenteds@gmail.com>
2018-08-24 17:17:47 -0300

    Merge function `awaitTask` into `await`

--- integration/kotlinx-coroutines-firebase-android/kotlinx-coroutines-firebase-android/src/main/java/kotlinx/coroutines/experimental/firebase/android/Task.kt
+++ integration/kotlinx-coroutines-firebase-android/kotlinx-coroutines-firebase-android/src/main/java/kotlinx/coroutines/experimental/firebase/android/Task.kt
@@ -2,24 +2,6 @@ package kotlinx.coroutines.experimental.firebase.android
 
 import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.experimental.suspendCancellableCoroutine
-import kotlin.coroutines.experimental.suspendCoroutine
-
-/**
- * Coroutine support to Firebase Task interface
- *
- * This method is not intended to be used in your code. You should probably use [await] sinsce it's
- * more idiomatic.
- *
- * The implementation is pretty simple. It justs use a [suspendCoroutine] to encapsulate the
- * Firebase [com.google.android.gms.tasks.OnCompleteListener] interface.
- *
- */
-private suspend fun <T> awaitTask(task: Task<T>): T = suspendCancellableCoroutine { continuation ->
-    task
-            .addOnSuccessListener(continuation::resume)
-            .addOnFailureListener(continuation::resumeWithException)
-            .addOnCanceledListener { continuation.cancel() }
-}
 
 /**
  * Coroutine support to Firebase Task interface
@@ -52,4 +34,9 @@ private suspend fun <T> awaitTask(task: Task<T>): T = suspendCancellableCoroutin
  * @throws Exception Thrown in case of network error or other reasons described in the Firebase docs
  * @return The value returned by the Firebase success callback
  */
-suspend fun <T> Task<T>.await(): T = awaitTask(this)
+suspend fun <T> Task<T>.await(): T = suspendCancellableCoroutine { continuation ->
+    task
+            .addOnSuccessListener(continuation::resume)
+            .addOnFailureListener(continuation::resumeWithException)
+            .addOnCanceledListener { continuation.cancel() }
+}
