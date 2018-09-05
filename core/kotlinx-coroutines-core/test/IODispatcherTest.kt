8fecc202bf7a8fa500d8ca4f92aa171ac2c25ec4
Roman Elizarov <elizarov@gmail.com>
2018-09-05 22:07:41 +0300

    Added missing package statements to tests

--- core/kotlinx-coroutines-core/test/IODispatcherTest.kt
+++ core/kotlinx-coroutines-core/test/IODispatcherTest.kt
@@ -2,7 +2,8 @@
  * Copyright 2016-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import kotlinx.coroutines.experimental.*
+package kotlinx.coroutines.experimental
+
 import org.junit.Test
 import kotlin.test.*
 
