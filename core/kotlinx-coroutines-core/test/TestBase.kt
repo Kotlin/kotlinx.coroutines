33e7ce44b7602d04d70e409b794e9da371b459cd
Roman Elizarov <elizarov@gmail.com>
2018-09-23 19:14:44 +0300

    Removed "cancel parent" from TestBase exception handlers

--- core/kotlinx-coroutines-core/test/TestBase.kt
+++ core/kotlinx-coroutines-core/test/TestBase.kt
@@ -146,7 +146,6 @@ public actual open class TestBase actual constructor() {
                     !unhandled[exCount - 1](e) ->
                         printError("Unhandled exception was unexpected: $e", e)
                 }
-                context[Job]?.cancel(e)
             })
         } catch (e: Throwable) {
             ex = e
