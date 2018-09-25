e29f49799d118b5c3feb38886af3030d027e7b60
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2018-09-25 15:45:53 +0300

    Avoid OOM in thread-pool dispatchers, try to reflectively invoke setRemoveFutureOnCancel on executor instance and use default dispatcher if attempt failed
    Fixes #571

--- core/kotlinx-coroutines-core/src/internal/Concurrent.kt
+++ core/kotlinx-coroutines-core/src/internal/Concurrent.kt
@@ -4,6 +4,7 @@
 
 package kotlinx.coroutines.experimental.internal
 
+import java.lang.reflect.*
 import java.util.*
 import java.util.concurrent.*
 import kotlin.concurrent.withLock as withLockJvm
@@ -16,3 +17,20 @@ internal actual typealias ReentrantLock = java.util.concurrent.locks.ReentrantLo
 internal actual inline fun <T> ReentrantLock.withLock(action: () -> T) = this.withLockJvm(action)
 
 internal actual fun <E> identitySet(expectedSize: Int): MutableSet<E> = Collections.newSetFromMap(IdentityHashMap(expectedSize))
+
+private val REMOVE_FUTURE_ON_CANCEL: Method? = try {
+    ScheduledThreadPoolExecutor::class.java.getMethod("setRemoveOnCancelPolicy", Boolean::class.java)
+} catch (e: Throwable) {
+    null
+}
+
+@Suppress("NAME_SHADOWING")
+internal fun removeFutureOnCancel(executor: Executor): Boolean {
+    try {
+        val executor = executor as? ScheduledExecutorService ?: return false
+        (REMOVE_FUTURE_ON_CANCEL ?: return false).invoke(executor, true)
+        return true
+    } catch (e: Throwable) {
+        return true
+    }
+}
