6d804d11e7d251021f5cd8f7b4b94f2bd2d6241e
Louis CAD <louis.cognault@gmail.com>
2018-10-06 19:47:37 +0200

    Use ConcurrentHasMap, and add documentation
    This commit also adds eager cancellation in case it is needed.

--- integration/kotlinx-coroutines-androidx-lifecycle/src/Lifecycle.kt
+++ integration/kotlinx-coroutines-androidx-lifecycle/src/Lifecycle.kt
@@ -2,24 +2,39 @@ package kotlinx.coroutines.androidx.lifecycle
 
 import androidx.lifecycle.GenericLifecycleObserver
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
+import androidx.lifecycle.Lifecycle.Event.*
 import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.Dispatchers
 import kotlinx.coroutines.experimental.Job
+import java.util.concurrent.ConcurrentHashMap
 
-fun Lifecycle.createJob(cancelEvent: Lifecycle.Event = ON_DESTROY): Job = Job().also { job ->
-    addObserver(object : GenericLifecycleObserver {
-        override fun onStateChanged(source: LifecycleOwner?, event: Lifecycle.Event) {
-            if (event == cancelEvent) {
-                removeObserver(this)
-                job.cancel()
+/**
+ * Creates a [Job] that will be cancelled when the [Lifecycle] encounters [cancelEvent]
+ * (which defaults to [ON_DESTROY]).
+ *
+ * Note that [cancelEvent] supports only "**down**" events. That means that only [ON_PAUSE],
+ * [ON_STOP] and [ON_DESTROY] are supported.
+ *
+ * If the [Lifecycle] is already destroyed, then the returned [Job] comes already cancelled.
+ */
+fun Lifecycle.createJob(cancelEvent: Lifecycle.Event = ON_DESTROY): Job {
+    require(cancelEvent in allowedCancelEvents) { "$cancelEvent is forbidden for createJob(…)." }
+    return Job().also { job ->
+        if (currentState == Lifecycle.State.DESTROYED) job.cancel()
+        else addObserver(object : GenericLifecycleObserver {
+            override fun onStateChanged(source: LifecycleOwner?, event: Lifecycle.Event) {
+                if (event == cancelEvent) {
+                    removeObserver(this)
+                    job.cancel()
+                }
             }
-        }
-    })
+        })
+    }
 }
 
-private val lifecycleJobs = mutableMapOf<Lifecycle, Job>()
+private val allowedCancelEvents = arrayOf(ON_PAUSE, ON_STOP, ON_DESTROY)
+private val lifecycleJobs = ConcurrentHashMap<Lifecycle, Job>()
 
 val Lifecycle.job: Job
     get() = lifecycleJobs[this] ?: createJob().also {
@@ -28,7 +43,7 @@ val Lifecycle.job: Job
             it.invokeOnCompletion { _ -> lifecycleJobs -= this }
         }
     }
-private val lifecycleCoroutineScopes = mutableMapOf<Lifecycle, CoroutineScope>()
+private val lifecycleCoroutineScopes = ConcurrentHashMap<Lifecycle, CoroutineScope>()
 
 val Lifecycle.coroutineScope: CoroutineScope
     get() = lifecycleCoroutineScopes[this] ?: job.let { job ->
