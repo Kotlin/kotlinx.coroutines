fd54bc4882357771d0bec29abbd6e99dfccab725
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2018-10-17 18:37:36 +0300

    Move undispatched thread-local event loop to dispatched continuation, execute all undispatched tasks in that loop to avoid SOE in user-supplied unconfined dispatchers and 'immediate' main dispatchers.

--- common/kotlinx-coroutines-core-common/src/Unconfined.kt
+++ common/kotlinx-coroutines-core-common/src/Unconfined.kt
@@ -4,44 +4,13 @@
 
 package kotlinx.coroutines
 
-import kotlinx.coroutines.internal.*
 import kotlin.coroutines.*
-import kotlin.jvm.*
 
 /**
  * A coroutine dispatcher that is not confined to any specific thread.
  */
-@NativeThreadLocal
 internal object Unconfined : CoroutineDispatcher() {
-    private data class State(@JvmField var isActive: Boolean = false,
-                             @JvmField val threadLocalQueue: ArrayList<Runnable> = ArrayList())
-    private val state = CommonThreadLocal { State() }
-
-    override fun dispatch(context: CoroutineContext, block: Runnable) {
-        // Stack-based event loop on top of thread-local arraylist
-        val state = state.get()
-        if (state.isActive) {
-            state.threadLocalQueue.add(block)
-            return
-        }
-
-        try {
-            state.isActive = true
-            block.run()
-            while (!state.threadLocalQueue.isEmpty()) {
-                val element = state.threadLocalQueue.removeAt(state.threadLocalQueue.lastIndex)
-                element.run()
-            }
-        } catch (e: Throwable) {
-            /*
-             * This exception doesn't happen normally, only if user either submitted throwing runnable
-             * or if we have a bug in implementation. Anyway, reset state of the dispatcher to the initial.
-             */
-            state.threadLocalQueue.clear()
-            throw DispatchException("Unexpected exception in Unconfined loop, clearing pending tasks", e)
-        } finally {
-            state.isActive = false
-        }
-    }
+    override fun isDispatchNeeded(context: CoroutineContext): Boolean = false
+    override fun dispatch(context: CoroutineContext, block: Runnable) { throw UnsupportedOperationException() }
     override fun toString(): String = "Unconfined"
 }
