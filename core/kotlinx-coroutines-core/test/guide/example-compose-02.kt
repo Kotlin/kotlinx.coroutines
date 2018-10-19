c1b1036c8c26ed2d7dd82ee06d4af6498a009d2d
Prendota <Prendota@mail.ru>
2018-10-19 10:55:34 +0300

    make compose examples runnable

--- core/kotlinx-coroutines-core/test/guide/example-compose-02.kt
+++ core/kotlinx-coroutines-core/test/guide/example-compose-02.kt
@@ -18,11 +18,26 @@ suspend fun doSomethingUsefulTwo(): Int {
     return 29
 }
 
-fun main(args: Array<String>) = runBlocking<Unit> {
+import kotlinx.coroutines.*
+import kotlin.system.*
+
+fun main() = runBlocking<Unit> {
+//sampleStart
     val time = measureTimeMillis {
         val one = async { doSomethingUsefulOne() }
         val two = async { doSomethingUsefulTwo() }
         println("The answer is ${one.await() + two.await()}")
     }
     println("Completed in $time ms")
+//sampleEnd    
+}
+
+suspend fun doSomethingUsefulOne(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
+
+suspend fun doSomethingUsefulTwo(): Int {
+    delay(1000L) // pretend we are doing something useful here, too
+    return 29
 }
