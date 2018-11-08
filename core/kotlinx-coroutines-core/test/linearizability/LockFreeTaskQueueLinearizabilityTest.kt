64ef99d313e281988684400a34825f721ebc4ede
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2018-11-08 12:47:22 +0300

    Update linearizability tests

--- core/kotlinx-coroutines-core/test/linearizability/LockFreeTaskQueueLinearizabilityTest.kt
+++ core/kotlinx-coroutines-core/test/linearizability/LockFreeTaskQueueLinearizabilityTest.kt
@@ -13,15 +13,14 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.internal.*
 import kotlin.test.*
 
+internal data class Snapshot(val elements: List<Int>, val isClosed: Boolean) {
+    constructor(q: LockFreeTaskQueue<Int>) : this(q.map { it }, q.isClosed())
+}
+
 @OpGroupConfig.OpGroupConfigs(OpGroupConfig(name = "consumer", nonParallel = true))
 @Param(name = "value", gen = IntGen::class, conf = "1:3")
-class LockFreeTaskQueueLinearizabilityTest : TestBase() {
-
-    private companion object {
-        var singleConsumer = false
-    }
-
-    private val q: LockFreeTaskQueue<Int> = LockFreeTaskQueue(singleConsumer)
+class LockFreeTaskQueueLinearizabilityTestSC : LockFreeTaskQueueLinearizabilityTestBase() {
+    private val q: LockFreeTaskQueue<Int> = LockFreeTaskQueue(singleConsumer = true)
 
     @Operation
     fun close() = q.close()
@@ -37,22 +36,58 @@ class LockFreeTaskQueueLinearizabilityTest : TestBase() {
 //    fun removeFirstOrNull() = q.removeFirstOrNull()
 
     @Test
-    fun testLinearizabilitySC() {
-        singleConsumer = true
-        linTest()
+    fun testSC() = linTest()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LockFreeTaskQueueLinearizabilityTestSC
+
+        return Snapshot(q) == Snapshot(other.q)
     }
+
+    override fun hashCode(): Int = Snapshot(q).hashCode()
+}
+
+@Param(name = "value", gen = IntGen::class, conf = "1:3")
+class LockFreeTaskQueueLinearizabilityTestMC : LockFreeTaskQueueLinearizabilityTestBase() {
+    private val q: LockFreeTaskQueue<Int> = LockFreeTaskQueue(singleConsumer = false)
+
+    @Operation
+    fun close() = q.close()
+
+    @Operation
+    fun addLast(@Param(name = "value") value: Int) = q.addLast(value)
+
+    /**
+     * Note, that removeFirstOrNull is not linearizable w.r.t. to addLast, so here
+     * we test only linearizability of close.
+     */
+//    @Operation(group = "consumer")
+//    fun removeFirstOrNull() = q.removeFirstOrNull()
 
     @Test
-    fun testLinearizabilityMC() {
-        singleConsumer = false
-        linTest()
+    fun testMC() = linTest()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LockFreeTaskQueueLinearizabilityTestMC
+
+        return Snapshot(q) == Snapshot(other.q)
     }
 
-    private fun linTest() {
+    override fun hashCode(): Int = Snapshot(q).hashCode()
+}
+
+open class LockFreeTaskQueueLinearizabilityTestBase : TestBase() {
+    fun linTest() {
         val options = StressOptions()
             .iterations(100 * stressTestMultiplierSqrt)
             .invocationsPerIteration(1000 * stressTestMultiplierSqrt)
-            .threads(3)
-        LinChecker.check(LockFreeTaskQueueLinearizabilityTest::class.java, options)
+            .threads(2)
+        LinChecker.check(this::class.java, options)
     }
 }
