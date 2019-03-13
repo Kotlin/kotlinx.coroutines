a5b52b3634e744fe50b4325f40cf04b11b066eb5
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-03-13 15:06:14 +0300

    Merge lazy getter with AndroidExceptionPreHandler to reduce class count

--- ui/kotlinx-coroutines-android/src/AndroidExceptionPreHandler.kt
+++ ui/kotlinx-coroutines-android/src/AndroidExceptionPreHandler.kt
@@ -10,20 +10,21 @@ import kotlinx.coroutines.*
 import java.lang.reflect.*
 import kotlin.coroutines.*
 
-private val getter by lazy {
-     try {
-         Thread::class.java.getDeclaredMethod("getUncaughtExceptionPreHandler").takeIf {
-             Modifier.isPublic(it.modifiers) && Modifier.isStatic(it.modifiers)
-         }
-     } catch (e: Throwable) {
-         null /* not found */
-     }
- }
-
 @Keep
 internal class AndroidExceptionPreHandler :
-    AbstractCoroutineContextElement(CoroutineExceptionHandler), CoroutineExceptionHandler
-{
+    AbstractCoroutineContextElement(CoroutineExceptionHandler), CoroutineExceptionHandler, Function0<Method?> {
+
+    private val preHandler by lazy(this)
+
+    // Reflectively lookup pre-handler. Implement Function0 to avoid generating second class for lambda
+    override fun invoke(): Method? = try {
+        Thread::class.java.getDeclaredMethod("getUncaughtExceptionPreHandler").takeIf {
+            Modifier.isPublic(it.modifiers) && Modifier.isStatic(it.modifiers)
+        }
+    } catch (e: Throwable) {
+        null /* not found */
+    }
+
     override fun handleException(context: CoroutineContext, exception: Throwable) {
         /*
          * If we are on old SDK, then use Android's `Thread.getUncaughtExceptionPreHandler()` that ensures that
@@ -38,7 +39,7 @@ internal class AndroidExceptionPreHandler :
         if (Build.VERSION.SDK_INT >= 28) {
             thread.uncaughtExceptionHandler.uncaughtException(thread, exception)
         } else {
-            (getter?.invoke(null) as? Thread.UncaughtExceptionHandler)
+            (preHandler?.invoke(null) as? Thread.UncaughtExceptionHandler)
                 ?.uncaughtException(thread, exception)
         }
     }
