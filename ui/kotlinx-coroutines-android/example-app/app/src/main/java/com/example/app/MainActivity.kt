5da72789670423a557e9b1fbf0808b87577ebc95
kokorev.yu <kokorev.yu@autodoc.ru>
2019-04-18 12:12:35 +0300

    android-example Add Empty Tack, Tack With Progress, Cancelable Tack

--- ui/kotlinx-coroutines-android/example-app/app/src/main/java/com/example/app/MainActivity.kt
+++ ui/kotlinx-coroutines-android/example-app/app/src/main/java/com/example/app/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.example.app
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        setup(hello, fab)
+
+        buttonEmptyTask.setOnClickListener { startEmptyTask() }
+        buttonTaskWithProgress.setOnClickListener { startTaskWithProgress() }
+        buttonCancelableTask.setOnClickListener { startCancelableTask() }
+        buttonCancelTask.setOnClickListener { cancelTask() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,8 +39,48 @@ class MainActivity : AppCompatActivity() {
         if (id == R.id.action_settings) return true
         return super.onOptionsItemSelected(item)
     }
-}
 
-fun setup(hello: TextView, fab: FloatingActionButton) {
-    // placeholder
+    private fun startEmptyTask() {
+        println("start empty task")
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(1000)
+            showText("Hello from Kotlin Coroutines!")
+        }
+    }
+
+    private fun startTaskWithProgress() {
+        println("start Empty With Progress")
+        GlobalScope.launch(Dispatchers.Main) {
+            progressBar.visibility = View.VISIBLE
+            delay(5000)
+            showText("Hello from Kotlin Coroutines!")
+            progressBar.visibility = View.GONE
+        }
+    }
+
+    @UseExperimental(InternalCoroutinesApi::class)
+    private fun startCancelableTask() {
+        println("start cancelable Task")
+        job = GlobalScope.launch(Dispatchers.Main) {
+            progressBar.visibility = View.VISIBLE
+            delay(5000)
+            showText("Hello from Kotlin Coroutines!")
+            progressBar.visibility = View.GONE
+        }
+        job?.invokeOnCompletion(true) {
+            if (it != null) {
+                job?.cancel()
+                showText("Cancelable Task was cancel")
+                progressBar.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun showText(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cancelTask() {
+        job?.cancel()
+    }
 }
