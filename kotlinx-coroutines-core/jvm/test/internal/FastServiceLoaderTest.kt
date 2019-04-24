be467e30f33b53f4564faf988241fd615277241b
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-04-24 13:07:07 +0300

    Move coroutine scope resources to android-unit-test project
    Fixes #1131

--- kotlinx-coroutines-core/jvm/test/internal/FastServiceLoaderTest.kt
+++ kotlinx-coroutines-core/jvm/test/internal/FastServiceLoaderTest.kt
@@ -6,13 +6,6 @@ import kotlin.test.*
 
 class FastServiceLoaderTest {
     @Test
-    fun testLoadingSameModuleService() {
-        val providers = Delay::class.java.let { FastServiceLoader.loadProviders(it, it.classLoader) }
-        assertEquals(1, providers.size)
-        assertEquals("kotlinx.coroutines.android.DelayImpl", providers[0].javaClass.name)
-    }
-
-    @Test
     fun testCrossModuleService() {
         val providers = CoroutineScope::class.java.let { FastServiceLoader.loadProviders(it, it.classLoader) }
         assertEquals(3, providers.size)
