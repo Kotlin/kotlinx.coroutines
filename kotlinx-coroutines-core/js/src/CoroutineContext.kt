234e3c3ff95aeedabf528def000353f020b03294
Roman Elizarov <elizarov@gmail.com>
2019-05-29 21:07:13 +0300

    ~ Remove unused "minusId"

--- kotlinx-coroutines-core/js/src/CoroutineContext.kt
+++ kotlinx-coroutines-core/js/src/CoroutineContext.kt
@@ -48,5 +48,3 @@ public actual fun CoroutineScope.newCoroutineContext(context: CoroutineContext):
 internal actual inline fun <T> withCoroutineContext(context: CoroutineContext, countOrElement: Any?, block: () -> T): T = block()
 internal actual fun Continuation<*>.toDebugString(): String = toString()
 internal actual val CoroutineContext.coroutineName: String? get() = null // not supported on JS
-@Suppress("NOTHING_TO_INLINE")
-internal actual inline fun CoroutineContext.minusId(): CoroutineContext = this
