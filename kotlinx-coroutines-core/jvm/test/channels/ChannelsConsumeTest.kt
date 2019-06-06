aa3d1ae0bffbf32fed55588fcea5ea6ac8b1d526
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-06-06 12:11:41 +0300

    Deprecate Channel operators

--- kotlinx-coroutines-core/jvm/test/channels/ChannelsConsumeTest.kt
+++ kotlinx-coroutines-core/jvm/test/channels/ChannelsConsumeTest.kt
@@ -2,6 +2,8 @@
  * Copyright 2016-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
+@file:Suppress("DEPRECATION")
+
 package kotlinx.coroutines.channels
 
 import kotlinx.coroutines.*
