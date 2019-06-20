a8d510c13123164e50a018bb83d4a97e12c470cb
Zach Klippenstein <zach.klippenstein@gmail.com>
2019-06-20 10:06:33 -0700

    Add distinctUntilChanged operator that uses a comparator function instead of a key extractor.

--- kotlinx-coroutines-core/common/test/flow/operators/DistinctUntilChangedTest.kt
+++ kotlinx-coroutines-core/common/test/flow/operators/DistinctUntilChangedTest.kt
@@ -33,6 +33,29 @@ class DistinctUntilChangedTest : TestBase() {
     }
 
     @Test
+    fun testDistinctUntilChangedAreEquivalent() = runTest {
+        val flow = flow {
+            emit(Box(1))
+            emit(Box(1))
+            emit(Box(2))
+            emit(Box(1))
+        }
+
+        val sum1 = flow.distinctUntilChanged().map { it.i }.sum()
+        val sum2 = flow.distinctUntilChanged { old, new -> old.i == new.i }.map { it.i }.sum()
+        assertEquals(5, sum1)
+        assertEquals(4, sum2)
+    }
+
+    @Test
+    fun testDistinctUntilChangedAreEquivalentSingleValue() = runTest {
+        val flow = flowOf(1)
+
+        val values = flow.distinctUntilChanged { _, _ -> fail("Expected not to compare single value.") }.toList()
+        assertEquals(listOf(1), values)
+    }
+
+    @Test
     fun testThrowingKeySelector() = runTest {
         val flow = flow {
             coroutineScope {
@@ -50,7 +73,25 @@ class DistinctUntilChangedTest : TestBase() {
     }
 
     @Test
-    fun testDistinctUntilChangedNull() = runTest{
+    fun testThrowingAreEquivalent() = runTest {
+        val flow = flow {
+            coroutineScope {
+                launch(start = CoroutineStart.ATOMIC) {
+                    hang { expect(3) }
+                }
+                expect(2)
+                emit(1)
+                emit(2)
+            }
+        }.distinctUntilChanged { _, _ -> throw TestException() }
+
+        expect(1)
+        assertFailsWith<TestException>(flow)
+        finish(4)
+    }
+
+    @Test
+    fun testDistinctUntilChangedNull() = runTest {
         val flow = flowOf(null, 1, null).distinctUntilChanged()
         assertEquals(listOf(null, 1, null), flow.toList())
     }
