ace5899869b15b774f20db2a463800540f748446
Zach Klippenstein <zach.klippenstein@gmail.com>
2019-06-20 10:06:33 -0700

    Add distinctUntilChanged operator that uses a comparator function instead of a key extractor.

--- kotlinx-coroutines-core/common/src/flow/operators/Distinct.kt
+++ kotlinx-coroutines-core/common/src/flow/operators/Distinct.kt
@@ -19,16 +19,36 @@ import kotlinx.coroutines.flow.unsafeFlow as flow
 public fun <T> Flow<T>.distinctUntilChanged(): Flow<T> = distinctUntilChangedBy { it }
 
 /**
+ * Returns flow where all subsequent repetitions of the same value are filtered out, when compared
+ * with each other via the provided [areEquivalent] function.
+ */
+@FlowPreview
+public fun <T> Flow<T>.distinctUntilChanged(areEquivalent: (old: T, new: T) -> Boolean): Flow<T> =
+    distinctUntilChangedBy(keySelector = { it }, areEquivalent = areEquivalent)
+
+/**
  * Returns flow where all subsequent repetitions of the same key are filtered out, where
  * key is extracted with [keySelector] function.
  */
 @FlowPreview
 public fun <T, K> Flow<T>.distinctUntilChangedBy(keySelector: (T) -> K): Flow<T> =
+    distinctUntilChangedBy(keySelector = keySelector, areEquivalent = { old, new -> old == new })
+
+/**
+ * Returns flow where all subsequent repetitions of the same key are filtered out, where
+ * keys are extracted with [keySelector] function and compared with each other via the
+ * provided [areEquivalent] function.
+ */
+private inline fun <T, K> Flow<T>.distinctUntilChangedBy(
+    crossinline keySelector: (T) -> K,
+    crossinline areEquivalent: (old: K, new: K) -> Boolean
+): Flow<T> =
     flow {
         var previousKey: Any? = NULL
         collect { value ->
             val key = keySelector(value)
-            if (previousKey != key) {
+            @Suppress("UNCHECKED_CAST")
+            if (previousKey === NULL || !areEquivalent(previousKey as K, key)) {
                 previousKey = key
                 emit(value)
             }
--- kotlinx-coroutines-core/common/test/flow/operators/DistinctUntilChangedTest.kt
+++ kotlinx-coroutines-core/common/test/flow/operators/DistinctUntilChangedTest.kt
@@ -33,6 +33,28 @@ class DistinctUntilChangedTest : TestBase() {
     }
 
     @Test
+    fun testDistinctUntilChangedAreEquivalent() = runTest {
+        val flow = flow {
+            emit(Box(1))
+            emit(Box(1))
+            emit(Box(2))
+            emit(Box(1))
+        }
+
+        val sum1 = flow.distinctUntilChanged().map { it.i }.sum()
+        val sum2 = flow.distinctUntilChanged { old, new -> old.i == new.i }.map { it.i }.sum()
+        assertEquals(5, sum1)
+        assertEquals(4, sum2)
+    }
+
+    @Test
+    fun testDistinctUntilChangedAreEquivalentSingleValue() = runTest {
+        val flow = flowOf(1)
+        val values = flow.distinctUntilChanged { _, _ -> fail("Expected not to compare single value.") }.toList()
+        assertEquals(listOf(1), values)
+    }
+
+    @Test
     fun testThrowingKeySelector() = runTest {
         val flow = flow {
             coroutineScope {
@@ -50,8 +72,26 @@ class DistinctUntilChangedTest : TestBase() {
     }
 
     @Test
-    fun testDistinctUntilChangedNull() = runTest{
-        val flow = flowOf(null, 1, null).distinctUntilChanged()
+    fun testThrowingAreEquivalent() = runTest {
+        val flow = flow {
+            coroutineScope {
+                launch(start = CoroutineStart.ATOMIC) {
+                    hang { expect(3) }
+                }
+                expect(2)
+                emit(1)
+                emit(2)
+            }
+        }.distinctUntilChanged { _, _ -> throw TestException() }
+
+        expect(1)
+        assertFailsWith<TestException>(flow)
+        finish(4)
+    }
+
+    @Test
+    fun testDistinctUntilChangedNull() = runTest {
+        val flow = flowOf(null, 1, null, null).distinctUntilChanged()
         assertEquals(listOf(null, 1, null), flow.toList())
     }
 }
