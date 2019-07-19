5414aa66bb2eb0132c4bd3840cb5b61598764fdb
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2019-07-19 11:25:08 +0300

    Finish proofs of the model of interrupts

--- formal-proofs/theories/lib/util/interruptibly.v
+++ formal-proofs/theories/lib/util/interruptibly.v
@@ -5,7 +5,7 @@ From iris.program_logic Require Export atomic.
 
 Definition new_handle: val := λ: <>, ref #0%nat.
 
-Definition interrupt: val := λ: "H", "H" <- #1%nat.
+Definition interrupt: val := λ: "H", CAS "H" #0%nat #1%nat ;; #().
 
 Definition wait: val :=
   rec: "wait" "H" "b" "e" := if: !"H" = #0%nat
@@ -64,7 +64,7 @@ Proof. apply _. Qed.
 Global Instance interrupt_sent_persistent γ: Persistent (interrupt_sent γ).
 Proof. apply _. Qed.
 
-Lemma wait_fail_spec : forall γ handle (b e: val),
+Lemma interruptibly_wait_fail_spec : forall γ handle (b e: val),
   {{{ is_interrupt_handle γ handle ∗ interrupt_sent γ }}}
     wait handle b e
   {{{ RET (InjLV #()); True }}}.
@@ -95,18 +95,26 @@ Proof.
     iDestruct "HInv" as (ℓ') "[>% Hn]".
     iDestruct "Hn" as (n') "[Hℓ [HOwn >%]]".
     inversion H1; subst.
-    admit.
-Admitted.
+    wp_store.
+    iMod (own_update with "HOwn") as "[HOwn HFrag]".
+    {
+      apply (auth_update_alloc _ (2%nat: mnatUR) (2%nat: mnatUR)).
+      apply mnat_local_update.
+      lia.
+    }
+    iMod ("HClose" with "[Hℓ HOwn]") as "_"; first by eauto 10 with iFrame.
+    iModIntro. wp_pures. by iApply "HPost".
+Qed.
 
-Lemma wait_spec : forall γ P Q handle (b e: val),
+Lemma interruptibly_wait_spec : forall γ P Q handle (b e: val),
   {{{ is_interrupt_handle γ handle ∗ P ∗
     ({{{ P }}}
       (b e)%V
-    {{{ r, RET r; ⌜r = InjLV #()⌝ ∧ P ∨ (∃ (v: val), ⌜r = InjRV v⌝) ∧ Q }}})
+    {{{ r, RET r; ⌜r = InjLV #()⌝ ∧ P ∨ (∃ (v: val), ⌜r = InjRV v⌝ ∧ Q v) }}})
   }}}
     wait handle b e
   {{{ r, RET r; ⌜r = InjLV #()⌝ ∧ P ∧ interrupted γ ∨
-                                        (∃ (v: val), ⌜r = InjRV v⌝ ∧ Q)}}}.
+                                        (∃ (v: val), ⌜r = InjRV v⌝ ∧ Q v )}}}.
 Proof.
   intros.
   iIntros "[#IntHInv [HP #Hbe]]"; rewrite /is_interrupt_handle.
@@ -122,15 +130,12 @@ Proof.
   all: iMod ("HClose" with "[Hℓ HOwn]") as "_"; first by eauto 10 with iFrame.
   all: iModIntro; wp_pures.
   - wp_bind (b e).
-    (* It can't infer Φ for some reason; applying it manually. *)
-    iSpecialize ("Hbe" $! (fun v => wp NotStuck top (
-      match: v with InjL <> => wait #ℓ b e | InjR "v" => InjR "v" end) Φ )).
     wp_apply ("Hbe" with "HP").
     iIntros (r) "H".
-    iDestruct "H" as "[[% HP]|[HR Q]]"; subst.
+    iDestruct "H" as "[[% HP]|HR]"; subst.
     * wp_pures.
       by wp_apply ("IH" with "HP").
-    * iDestruct "HR" as (v) "%"; subst.
+    * iDestruct "HR" as (v) "[% HQ]"; subst.
       wp_pures.
       iApply "HPost".
       eauto.
@@ -154,5 +159,132 @@ Proof.
     rewrite /interrupted.
     eauto with iFrame.
 Qed.
+
+Lemma interruptibly_fail_spec : forall γ P Q' handle (b h e: val),
+  {{{ is_interrupt_handle γ handle ∗ P ∗ interrupt_sent γ ∗
+    {{{ P }}}
+      (h e)%V
+    {{{ (r: val), RET r; Q' r }}}
+  }}}
+    interruptibly handle b h e
+  {{{ (v: val), RET (#true, v)%V; Q' v }}}.
+Proof.
+  intros.
+  iIntros "[#HIsInt [HP [#HIntSent #Hhe]]] HPost".
+  wp_rec.
+  wp_pures.
+  wp_apply (interruptibly_wait_fail_spec); auto.
+  iIntros "_".
+  wp_pures.
+  wp_bind (h e).
+  wp_apply ("Hhe" with "HP").
+  iIntros (r) "HQ'".
+  wp_pures.
+  by iApply "HPost".
+Qed.
+
+Lemma interruptibly_spec : forall γ P Q Q' handle (b h e: val),
+  {{{ is_interrupt_handle γ handle ∗ P ∗
+    {{{ P }}}
+      (b e)%V
+    {{{ r, RET r; ⌜r = InjLV #()⌝ ∧ P ∨ (∃ (v: val), ⌜r = InjRV v⌝ ∧ Q v) }}} ∗
+    {{{ P }}}
+      (h e)%V
+    {{{ (r: val), RET r; Q' r }}}
+  }}}
+    interruptibly handle b h e
+  {{{ r v, RET r; ⌜r = (#true, v)%V⌝ ∧ Q' v ∨ ⌜r = (#false, InjRV v)%V⌝ ∧ Q v }}}.
+Proof.
+  intros.
+  iIntros "[#HIsInt [HP [#Hbe #Hhe]]] HPost".
+  wp_rec.
+  wp_pures.
+  wp_bind (wait _ _ _).
+  wp_apply (interruptibly_wait_spec with "[HP]").
+  { iSplitR; eauto.
+    iSplitL. iApply "HP".
+    iApply "Hbe". }
+  iIntros (r) "HP".
+  iDestruct "HP" as "[[% [HP HIntr]] | Hr]".
+  - subst. wp_pures.
+    wp_bind (h e).
+    wp_apply ("Hhe" with "HP").
+    iIntros (r) "HQ'".
+    wp_pures.
+    iApply "HPost".
+    iLeft.
+    by iFrame.
+  - iDestruct "Hr" as (v) "[% HQ]".
+    subst. wp_pures.
+    iApply "HPost".
+    iRight.
+    by iFrame.
+Qed.
+
+Lemma new_handle_spec:
+  {{{ True }}}
+    new_handle #()
+  {{{ γ H, RET H; is_interrupt_handle γ H }}}.
+Proof.
+  iIntros (Φ) "_ HPost". rewrite /new_handle -wp_fupd.
+  wp_lam. rewrite /is_interrupt_handle.
+  iMod (own_alloc (● (O : mnatUR))) as (γ) "HOwn".
+  { apply auth_valid_discrete. simpl.
+    split. done.
+    by eexists.
+  }
+  wp_alloc ℓ as "Hℓ".
+  iMod (inv_alloc N _ (interrupt_handle_inv γ #ℓ) with "[HOwn Hℓ]") as "HInv".
+  {
+    iNext. rewrite /interrupt_handle_inv. by eauto with iFrame.
+  }
+  iModIntro.
+  by iApply "HPost".
+Qed.
+
+Lemma interrupt_spec γ handle:
+  {{{ is_interrupt_handle γ handle }}}
+    interrupt handle
+  {{{ v, RET v; interrupt_sent γ }}}.
+Proof.
+  iIntros (Φ) "#HIsInt HPost".
+  rewrite /is_interrupt_handle /interrupt.
+  wp_lam. wp_bind (CmpXchg _ _ _).
+  iInv N as "HInv" "HClose".
+  iDestruct "HInv" as (ℓ) "[>% Hn]". subst.
+  iDestruct "Hn" as (n) "[Hℓ [HOwn >%]]".
+  destruct n.
+  - wp_cmpxchg_suc.
+    iMod (own_update with "HOwn") as "[HOwn HFrag]".
+    {
+      apply (auth_update_alloc _ (1%nat: mnatUR) (1%nat: mnatUR)).
+      apply mnat_local_update.
+      lia.
+    }
+    iMod ("HClose" with "[Hℓ HOwn]") as "_".
+    {
+      iNext; iExists ℓ; iSplitR; auto.
+      iExists (S O); iFrame. auto.
+    }
+    iModIntro.
+    wp_pures.
+    iApply "HPost".
+    by rewrite /interrupt_sent.
+  - wp_cmpxchg_fail; first done.
+    iMod (own_update with "HOwn") as "[HOwn HFrag]".
+    {
+      apply (auth_update_core_id _ (1%nat: mnatUR)).
+      apply mnat_included.
+      lia.
+    }
+    iMod ("HClose" with "[Hℓ HOwn]") as "_".
+    {
+      iExists _. eauto 10 with iFrame.
+    }
+    iModIntro.
+    wp_pures.
+    iApply "HPost".
+    by rewrite /interrupt_sent.
+ Qed.
 
 End interruptibly_proof.
