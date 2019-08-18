74241a4f9fabd3c9613ec5805477d55616030a47
Yanis Batura <yanis.batura@gmail.com>
2019-08-18 18:50:45 +0700

    Improve docs / fix grammar in Yield.kt

--- kotlinx-coroutines-core/common/src/Yield.kt
+++ kotlinx-coroutines-core/common/src/Yield.kt
@@ -8,12 +8,12 @@ import kotlin.coroutines.*
 import kotlin.coroutines.intrinsics.*
 
 /**
- * Yields a thread (or thread pool) of the current coroutine dispatcher to other coroutines to run.
- * If the coroutine dispatcher does not have its own thread pool (like [Dispatchers.Unconfined]) then this
- * function does nothing, but checks if the coroutine [Job] was completed.
+ * Yields the thread (or thread pool) of the current coroutine dispatcher to other coroutines to run.
+ * If the coroutine dispatcher does not have its own thread pool (like [Dispatchers.Unconfined]), this
+ * function does nothing but check if the coroutine's [Job] was completed.
  * This suspending function is cancellable.
  * If the [Job] of the current coroutine is cancelled or completed when this suspending function is invoked or while
- * this function is waiting for dispatching, it resumes with [CancellationException].
+ * this function is waiting for dispatch, it resumes with a [CancellationException].
  */
 public suspend fun yield(): Unit = suspendCoroutineUninterceptedOrReturn sc@ { uCont ->
     val context = uCont.context
