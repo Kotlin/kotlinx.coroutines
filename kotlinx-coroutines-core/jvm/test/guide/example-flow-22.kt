c3af249ef293599f76c7d8ed1865dde4dc627d19
Adam Szadkowski <klyman89@gmail.com>
2019-08-31 11:25:14 +0200

    Fix incorrect currentTimeMillis() call

--- kotlinx-coroutines-core/jvm/test/guide/example-flow-22.kt
+++ kotlinx-coroutines-core/jvm/test/guide/example-flow-22.kt
@@ -11,9 +11,9 @@ import kotlinx.coroutines.flow.*
 fun main() = runBlocking<Unit> { 
     val nums = (1..3).asFlow().onEach { delay(300) } // numbers 1..3 every 300 ms
     val strs = flowOf("one", "two", "three").onEach { delay(400) } // strings every 400 ms          
-    val startTime = currentTimeMillis() // remember the start time 
+    val startTime = System.currentTimeMillis() // remember the start time 
     nums.combine(strs) { a, b -> "$a -> $b" } // compose a single string with "combine"
         .collect { value -> // collect and print 
-            println("$value at ${currentTimeMillis() - startTime} ms from start") 
+            println("$value at ${System.currentTimeMillis() - startTime} ms from start") 
         } 
 }
