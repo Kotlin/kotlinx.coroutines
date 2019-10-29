a4c095e32d10efc1a97dd3da15820d11a8e41cc6
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-10-29 19:48:49 +0300

    Fix ReactorFlow integration bug exposed by TestBase

--- reactive/kotlinx-coroutines-reactor/test/FlowAsFluxTest.kt
+++ reactive/kotlinx-coroutines-reactor/test/FlowAsFluxTest.kt
@@ -10,18 +10,18 @@ import kotlin.test.assertEquals
 
 class FlowAsFluxTest : TestBase() {
     @Test
-    fun testFlowToFluxContextPropagation() = runBlocking<Unit> {
+    fun testFlowToFluxContextPropagation() {
         val flux = flow<String> {
-            (1..4).forEach { i -> emit(m(i).awaitFirst()) }
+            (1..4).forEach { i -> emit(createMono(i).awaitFirst()) }
         }   .asFlux()
             .subscriberContext(Context.of(1, "1"))
             .subscriberContext(Context.of(2, "2", 3, "3", 4, "4"))
-        var i = 0
-        flux.subscribe { str -> i++; println(str); assertEquals(str, i.toString()) }
+        val list = flux.collectList().block()!!
+        assertEquals(listOf("1", "2", "3", "4"), list)
     }
 
-    private fun m(i: Int): Mono<String> = mono {
-        val ctx = coroutineContext[ReactorContext]?.context
-        ctx?.getOrDefault(i, "noValue")
+    private fun createMono(i: Int): Mono<String> = mono {
+        val ctx = coroutineContext[ReactorContext]!!.context
+        ctx.getOrDefault(i, "noValue")
     }
 }
