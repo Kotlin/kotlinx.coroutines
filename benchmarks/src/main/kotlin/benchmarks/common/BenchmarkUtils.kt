ee751e746ff275de58bbad4ff5125d641087e651
fmixing <alicekoroleva239@gmail.com>
2019-11-13 14:09:28 +0300

    Rebased on the flatten-flow-bench

--- benchmarks/src/main/kotlin/benchmarks/common/BenchmarkUtils.kt
+++ benchmarks/src/main/kotlin/benchmarks/common/BenchmarkUtils.kt
@@ -4,6 +4,7 @@
 
 package benchmarks.common
 
+import java.nio.file.*
 import java.util.concurrent.*
 
 fun doGeomDistrWork(work: Int) {
@@ -15,3 +16,22 @@ fun doGeomDistrWork(work: Int) {
         if (r.nextDouble() < p) break
     }
 }
+
+// Runs the specified process, waits until it finishes, and returns its exit code.
+fun runProcess(classNameToRun : String, jvmOptions : List<String>, args : Array<String>) : Int {
+    val runJavaProcessCommand = ArrayList<String>()
+    runJavaProcessCommand.run {
+        this.add(JAVA_PATH)
+        this.addAll(jvmOptions)
+        this.add("-cp")
+        this.add(CLASS_PATH)
+        this.add(classNameToRun)
+        this.addAll(args)
+    }
+    val processBuilder = ProcessBuilder(runJavaProcessCommand)
+    val process = processBuilder.inheritIO().redirectError(ProcessBuilder.Redirect.INHERIT).start()
+    return process.waitFor()
+}
+
+private val CLASS_PATH = System.getProperty("java.class.path")
+private val JAVA_PATH = Paths.get(System.getProperty("java.home")).resolve("bin").resolve("java").toString()
