8b2a6a9faf4270f41380c83231f0fd778e7f5f98
Roman Elizarov <elizarov@gmail.com>
2019-11-23 18:21:22 +0300

    Avoid potential StackOverflow on wrapped UnconfinedDispatcher+yield
    See ImmediateYieldTest.testWrappedUnconfinedDispatcherYieldStackOverflow
    
    This commit changes the way Unconfined dispatcher is detected.
    Instead of equality check we try to dispatch to the dispatcher with
    a specially updated coroutine context that has YieldContext element
    that is processed by the Unconfied dispatcher in a special way.

--- kotlinx-coroutines-core/common/src/Unconfined.kt
+++ kotlinx-coroutines-core/common/src/Unconfined.kt
@@ -5,13 +5,35 @@
 package kotlinx.coroutines
 
 import kotlin.coroutines.*
+import kotlin.jvm.*
 
 /**
  * A coroutine dispatcher that is not confined to any specific thread.
  */
 internal object Unconfined : CoroutineDispatcher() {
     override fun isDispatchNeeded(context: CoroutineContext): Boolean = false
-    // Just in case somebody wraps Unconfined dispatcher casing the "dispatch" to be called from "yield"
-    override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
+
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        // Just in case somebody wraps Unconfined dispatcher casing the "dispatch" to be called from "yield"
+        // See also code of "yield" function
+        val yieldContext = context[YieldContext]
+        if (yieldContext != null) {
+            // report to "yield" that it is an unconfined dispatcher and don't call "block.run()"
+            yieldContext.dispatcherWasUnconfined = true
+            return
+        }
+        block.run()
+    }
+    
     override fun toString(): String = "Unconfined"
+}
+
+/**
+ * Used to detect calls to [Unconfined.dispatch] from [yield] function.
+ */
+internal class YieldContext : AbstractCoroutineContextElement(Key) {
+    companion object Key : CoroutineContext.Key<YieldContext>
+
+    @JvmField
+    var dispatcherWasUnconfined = false
 }
