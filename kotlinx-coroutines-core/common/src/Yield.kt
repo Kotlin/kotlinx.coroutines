22fe6c7febf8c932e6a1f77ed4c4825d20de3480
Roman Elizarov <elizarov@gmail.com>
2019-11-26 11:33:08 +0300

    Optimize yield via dispatcher.isDispatchNeeded
    Also, as it was before, throw UnsupportedOperationException from the
    Dispatcher.Unconfined.dispatch method in case some code wraps
    the Unconfined dispatcher but fails to delegate isDispatchNeeded
    properly.

--- kotlinx-coroutines-core/common/src/Yield.kt
+++ kotlinx-coroutines-core/common/src/Yield.kt
@@ -20,23 +20,30 @@ import kotlin.coroutines.intrinsics.*
  *
  * If the coroutine dispatcher is [Unconfined][Dispatchers.Unconfined], this
  * functions suspends only when there are other unconfined coroutines working and forming an event-loop.
- * For other dispatchers, this function does not call [CoroutineDispatcher.isDispatchNeeded] and
- * always suspends to be resumed later. If there is no [CoroutineDispatcher] in the context, it does not suspend.
+ * For other dispatchers, this function calls [CoroutineDispatcher.dispatch] and
+ * always suspends to be resumed later regardless of the result of [CoroutineDispatcher.isDispatchNeeded].
+ * If there is no [CoroutineDispatcher] in the context, it does not suspend.
  */
 public suspend fun yield(): Unit = suspendCoroutineUninterceptedOrReturn sc@ { uCont ->
     val context = uCont.context
     context.checkCompletion()
     val cont = uCont.intercepted() as? DispatchedContinuation<Unit> ?: return@sc Unit
-    // This code detects the Unconfined dispatcher even if it was wrapped into another dispatcher
-    val yieldContext = YieldContext()
-    cont.dispatchYield(context + yieldContext, Unit)
-    // Special case for the unconfined dispatcher that can yield only in existing unconfined loop
-    if (yieldContext.dispatcherWasUnconfined) {
-        // Means that the Unconfined dispatcher got the call, but did not do anything.
-        // See also code of "Unconfined.dispatch" function.
-        return@sc if (cont.yieldUndispatched()) COROUTINE_SUSPENDED else Unit
+    if (cont.dispatcher.isDispatchNeeded(context)) {
+        // this is a regular dispatcher -- do simple dispatchYield
+        cont.dispatchYield(context, Unit)
+    } else {
+        // This is either an "immediate" dispatcher or the Unconfined dispatcher
+        // This code detects the Unconfined dispatcher even if it was wrapped into another dispatcher
+        val yieldContext = YieldContext()
+        cont.dispatchYield(context + yieldContext, Unit)
+        // Special case for the unconfined dispatcher that can yield only in existing unconfined loop
+        if (yieldContext.dispatcherWasUnconfined) {
+            // Means that the Unconfined dispatcher got the call, but did not do anything.
+            // See also code of "Unconfined.dispatch" function.
+            return@sc if (cont.yieldUndispatched()) COROUTINE_SUSPENDED else Unit
+        }
+        // Otherwise, it was some other dispatcher that successfully dispatched the coroutine
     }
-    // It was some other dispatcher that successfully dispatched the coroutine
     COROUTINE_SUSPENDED
 }
 
