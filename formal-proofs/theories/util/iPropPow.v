6c18c798a1e7adaafd16b73480d1ab1664d4efce
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2019-12-17 17:40:10 +0300

    Remove the proofs written for training

--- formal-proofs/theories/util/iPropPow.v
+++
@@ -1,8 +0,0 @@
-Require Import iris.bi.interface.
-Require Import iris.base_logic.lib.iprop.
-
-Fixpoint iPropPow {Σ} (n : nat) (p : iProp Σ) : iProp Σ :=
-  match n with
-  | O => (True)%I
-  | S n => (p ∗ iPropPow n p)%I
-  end.
