e153863c8beb4448df527a3006b3ef00f2d5e93f
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-02-10 21:12:58 +0300

    Integration with binary-compatibility-validator plugin (#1790)

--- binary-compatibility-validator/test/cases/internal/internalMultifile1.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2016-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-
-@file:JvmName("MultifileKt")
-@file:JvmMultifileClass
-package cases.internal
-
-internal fun internalFun1() = internalVal
