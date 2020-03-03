ca1093cf50fa68c053fcdbd957da2b60c1ef850a
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-03-03 11:56:55 +0300

    Restore AsyncJvmTest

--- kotlinx-coroutines-core/jvm/test/AsyncJvmTest.kt
+++ kotlinx-coroutines-core/jvm/test/AsyncJvmTest.kt
@@ -6,16 +6,41 @@ package kotlinx.coroutines
 
 import kotlin.test.*
 
+
 class AsyncJvmTest : TestBase() {
     // This must be a common test but it fails on JS because of KT-21961
     @Test
     fun testAsyncWithFinally() = runTest {
-         launch(Dispatchers.Default) {
+        expect(1)
 
-         }
-
-        launch(Dispatchers.IO) {
-
+        @Suppress("UNREACHABLE_CODE")
+        val d = async {
+            expect(3)
+            try {
+                yield() // to main, will cancel
+            } finally {
+                expect(6) // will go there on await
+                return@async "Fail" // result will not override cancellation
+            }
+            expectUnreached()
+            "Fail2"
         }
+        expect(2)
+        yield() // to async
+        expect(4)
+        check(d.isActive && !d.isCompleted && !d.isCancelled)
+        d.cancel()
+        check(!d.isActive && !d.isCompleted && d.isCancelled)
+        check(!d.isActive && !d.isCompleted && d.isCancelled)
+        expect(5)
+        try {
+            d.await() // awaits
+            expectUnreached() // does not complete normally
+        } catch (e: Throwable) {
+            expect(7)
+            check(e is CancellationException)
+        }
+        check(!d.isActive && d.isCompleted && d.isCancelled)
+        finish(8)
     }
 }
