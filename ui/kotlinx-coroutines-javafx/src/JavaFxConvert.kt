43e5f9766ea58ac7490bfe17b2fe6c9e362c71e5
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2020-03-04 14:08:00 +0300

    Fix wording in documentation

--- ui/kotlinx-coroutines-javafx/src/JavaFxConvert.kt
+++ ui/kotlinx-coroutines-javafx/src/JavaFxConvert.kt
@@ -11,18 +11,12 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
 
 /**
- * Creates an instance of a cold [Flow] that subscribes to the given [ObservableValue] and produces
- * its values as they change.
- *
- * The resulting flow is conflated, meaning that if several values arrive in a quick succession, only
- * the last one will be produced.
- *
- * It produces at least one value.
- *
+ * Creates an instance of a cold [Flow] that subscribes to the given [ObservableValue] and emits
+ * its values as they change. The resulting flow is conflated, meaning that if several values arrive in quick
+ * succession, only the last one will be emitted.
  * Since this implementation uses [ObservableValue.addListener], even if this [ObservableValue]
  * supports lazy evaluation, eager computation will be enforced while the flow is being collected.
- *
- * All the calls to JavaFX API are performed in the JavaFX application thread.
+ * All the calls to JavaFX API are performed in [Dispatchers.JavaFx].
  *
  * ### Operator fusion
  *
