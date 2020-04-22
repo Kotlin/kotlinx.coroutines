68021a8039927b6ba8db708db94d0362d976b3fc
Victor Turansky <victor.turansky@gmail.com>
2020-04-23 00:38:41 +0300

    Kotlin DSL - 'android-unit-tests'

---
+++ ui/kotlinx-coroutines-android/android-unit-tests/build.gradle.kts
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+dependencies {
+    testImplementation("com.google.android:android:${property("android_version")}")
+    testImplementation("com.android.support:support-annotations:${property("android_support_version")}")
+    testImplementation("org.robolectric:robolectric:${property("robolectric_version")}")
+    testImplementation(project(":kotlinx-coroutines-test"))
+    testImplementation(project(":kotlinx-coroutines-android"))
+}
