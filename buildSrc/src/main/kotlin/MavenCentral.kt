e26021ae0a6666018a1f39e07699fcc3ff162a16
Victor Turansky <victor.turansky@gmail.com>
2020-04-27 14:51:42 +0300

    Kotlin DSL - 'maven-central' (#1938) (#1953)

---
+++ buildSrc/src/main/kotlin/MavenCentral.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import org.gradle.api.publish.maven.MavenPom
+
+// --------------- pom configuration ---------------
+
+fun MavenPom.configureMavenCentralMetadata(project: Project) {
+    name by project.name
+    description by "Coroutines support libraries for Kotlin"
+    url by "https://github.com/Kotlin/kotlinx.coroutines"
+
+    licenses {
+        license {
+            name by "The Apache Software License, Version 2.0"
+            url by "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            distribution by "repo"
+        }
+    }
+
+    developers {
+        developer {
+            id by "JetBrains"
+            name by "JetBrains Team"
+            organization by "JetBrains"
+            organizationUrl by "https://www.jetbrains.com"
+        }
+    }
+
+    scm {
+        url by "https://github.com/Kotlin/kotlinx.coroutines"
+    }
+}
+
+private infix fun <T> Property<T>.by(value: T) {
+    set(value)
+}
