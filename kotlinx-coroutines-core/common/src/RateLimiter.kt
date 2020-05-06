1e140e64da6293dfb5c17fe9b3464fd35aa26a2c
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2020-05-06 16:02:36 +0300

    Remove `Reservation` from RateLimiter; add constructors
    Almost all use cases for a `Reservation` can be implemented using
    standard coroutine facilities, so it was dropped.

--- kotlinx-coroutines-core/common/src/RateLimiter.kt
+++ kotlinx-coroutines-core/common/src/RateLimiter.kt
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
 package kotlinx.coroutines
 
 import kotlinx.atomicfu.*
@@ -6,66 +9,43 @@ import kotlin.math.*
 import kotlin.time.*
 
 public abstract class RateLimiter {
-    @ExperimentalTime
-    internal abstract fun requestTimeSlot(atTime: Long, tokens: Long, maxDelay: Duration): Long
-    internal abstract fun returnTokens(atTime: Long, takenAtTime: Long, tokens: Long)
-    internal abstract fun addTokens(atTime: Long, tokens: Long)
-}
+    protected abstract fun requestTimeSlot(atTime: Long, tokens: Long, maxDelay: Long): Long
+    protected abstract fun relinquishTokens(atTime: Long, takenAtTime: Long, tokens: Long)
+    protected abstract fun addTokens(atTime: Long, tokens: Long)
 
-public fun RateLimiter.addTokens(tokens: Long): Unit = addTokens(nanoTime(), tokens)
-public fun RateLimiter.consumeTokens(tokens: Long): Unit = addTokens(-tokens)
-
-@ExperimentalTime
-public fun RateLimiter.tryConsume(tokens: Long = 1): Boolean {
-    return requestTimeSlot(nanoTime(), tokens, Duration.ZERO) != Long.MAX_VALUE
-}
-
-@ExperimentalTime
-public suspend fun RateLimiter.consume(tokens: Long = 1, maxDelay: Duration = Duration.INFINITE) {
-    val currentTime = nanoTime()
-    val nextSlot = requestTimeSlot(currentTime, tokens, maxDelay)
-    if (nextSlot == Long.MAX_VALUE) {
-        throw UnsupportedOperationException("Could not allocate $tokens tokens in $maxDelay")
-    }
-    val toSleep = nextSlot - currentTime
-    delay(toSleep)
-}
-
-public class Reservation internal constructor (private val tokens: Long, private val timeToAct: Long, private val limiter: RateLimiter) {
-
-    private enum class State {
-        INITIAL,
-        CANCELLED,
-        ACTED_ON
-    }
-
-    private val state: AtomicRef<State> = atomic(State.INITIAL)
-
-    public suspend fun wait() {
-        state.compareAndSet(State.INITIAL, State.ACTED_ON)
+    private suspend fun consume(tokens: Long, maxDelay: Long) {
+        require(tokens > 0)
         val currentTime = nanoTime()
-        val toSleep = timeToAct - currentTime
-        delay(toSleep)
+        val nextSlot = requestTimeSlot(currentTime, tokens, maxDelay)
+        if (nextSlot == Long.MAX_VALUE) {
+            throw UnsupportedOperationException("Could not allocate $tokens tokens to be available in $maxDelay")
+        }
+        val toSleep = nextSlot - currentTime
+        try {
+            delay(toSleep)
+        } catch (e: CancellationException) {
+            relinquishTokens(nanoTime(), currentTime, tokens)
+        }
     }
 
-    public suspend fun cancel() {
-        state.compareAndSet(State.INITIAL, State.CANCELLED)
-        limiter.returnTokens(nanoTime(), timeToAct, tokens)
-    }
-}
+    public fun addTokens(tokens: Long): Unit = addTokens(nanoTime(), tokens)
+    public fun consumeTokens(tokens: Long): Unit = addTokens(-tokens)
 
-@ExperimentalTime
-public suspend fun RateLimiter.getReservation(tokens: Long = 1, maxDelay: Duration = Duration.INFINITE): Reservation? {
-    val nextSlot = requestTimeSlot(nanoTime(), tokens, maxDelay)
-    if (nextSlot == Long.MAX_VALUE) {
-        return null
+    public fun tryConsume(tokens: Long = 1): Boolean {
+        require(tokens > 0)
+        return requestTimeSlot(nanoTime(), tokens, 0) != Long.MAX_VALUE
     }
-    return Reservation(tokens, nextSlot, this)
+
+    public suspend fun consume(tokens: Long = 1): Unit =
+        consume(tokens, Long.MAX_VALUE)
+
+    @ExperimentalTime
+    public suspend fun consume(tokens: Long, maxDelay: Duration): Unit =
+        consume(tokens, maxDelay.toLongNanoseconds())
 }
 
 internal class RateLimitedFlow<T>(
     val flow: Flow<T>, private val rateLimiter: RateLimiter, private val tokensPerValue: (T) -> Long): Flow<T> {
-    @ExperimentalTime
     @InternalCoroutinesApi
     override suspend fun collect(collector: FlowCollector<T>) {
         flow.collect {
@@ -78,11 +58,17 @@ internal class RateLimitedFlow<T>(
 public fun <T> Flow<T>.rateLimit(rateLimiter: RateLimiter, tokensPerValue: (T) -> Long): Flow<T> =
     RateLimitedFlow(this, rateLimiter, tokensPerValue)
 
-internal class TokenBucketRateLimiter(bandwidths: Array<Bandwidth>): RateLimiter() {
-
-    internal class Bandwidth(
-        val capacity: Long, val refillPeriod: Long, val quantum: Long = 1, val initialAmount: Long = capacity) {
+internal data class Bandwidth(
+    val capacity: Long, val refillPeriod: Long, val quantum: Long = 1, val initialAmount: Long = capacity) {
+    init {
+        require(capacity > 0)
+        require(refillPeriod > 0)
+        require(quantum > 0)
+        require(initialAmount >= 0)
     }
+}
+
+internal class TokenBucketRateLimiter(bandwidths: Array<Bandwidth>): RateLimiter() {
 
     internal class State(internal val bandwidths: Array<Bandwidth>,
                          private val tokensPerBandwidth: Array<Long> =
@@ -103,6 +89,9 @@ internal class TokenBucketRateLimiter(bandwidths: Array<Bandwidth>): RateLimiter
             for (i in 0..bandwidths.size) {
                 val bandwidth = bandwidths[i]
                 val previousUpdate = lastUpdateTimes[i]
+                if (atTime <= previousUpdate) {
+                    continue
+                }
                 val completePeriods = (atTime - previousUpdate) / bandwidth.refillPeriod
                 lastUpdateTimes[i] = previousUpdate + completePeriods * bandwidth.refillPeriod // <= atTime, no overflow
                 val currentTokens = tokensPerBandwidth[i] + completePeriods * bandwidth.quantum // TODO: check overflow
@@ -128,11 +117,11 @@ internal class TokenBucketRateLimiter(bandwidths: Array<Bandwidth>): RateLimiter
         fun nextDeficitCompensationTime(tokens: Long): Long = (0..bandwidths.size)
             .map { i ->
                 val currentTokens = tokensPerBandwidth[i]
-                val timeToWait = if (currentTokens >= tokens) {
+                val timeToWaitSinceLastUpdate = if (currentTokens >= tokens) {
                     0L
                 } else {
                     val deficit = tokens - currentTokens
-                    if (deficit <= 0) { // an overflow happened
+                    if (deficit <= 0) { // TODO: an overflow happened
                         return@map 0L
                     }
                     val bandwidth = bandwidths[i]
@@ -142,7 +131,7 @@ internal class TokenBucketRateLimiter(bandwidths: Array<Bandwidth>): RateLimiter
                     }
                     periods * bandwidth.refillPeriod
                 }
-                lastUpdateTimes[i] + timeToWait
+                lastUpdateTimes[i] + timeToWaitSinceLastUpdate
             }
             .reduce { a, b -> max(a, b) }
 
@@ -176,15 +165,14 @@ internal class TokenBucketRateLimiter(bandwidths: Array<Bandwidth>): RateLimiter
         }
     }
 
-    @ExperimentalTime
-    override fun requestTimeSlot(atTime: Long, tokens: Long, maxDelay: Duration): Long = withState { currentState ->
+    override fun requestTimeSlot(atTime: Long, tokens: Long, maxDelay: Long): Long = withState { currentState ->
         require(tokens > 0)
         currentState.refill(atTime)
         val deficitCompensationTime = currentState.nextDeficitCompensationTime(tokens)
         if (deficitCompensationTime == Long.MAX_VALUE) {
             return Long.MAX_VALUE
         }
-        if (deficitCompensationTime - atTime > maxDelay.toLongNanoseconds()) {
+        if (deficitCompensationTime - atTime > maxDelay) {
             return Long.MAX_VALUE
         }
         currentState.consume(tokens)
@@ -196,11 +184,42 @@ internal class TokenBucketRateLimiter(bandwidths: Array<Bandwidth>): RateLimiter
         currentState.addTokens(tokens)
     }
 
-    override fun returnTokens(atTime: Long, takenAtTime: Long, tokens: Long) = withState { currentState ->
+    override fun relinquishTokens(atTime: Long, takenAtTime: Long, tokens: Long) = withState { currentState ->
         require(tokens > 0)
         currentState.refill(atTime)
         if (!currentState.returnTokens(takenAtTime, tokens)) {
             return // do not loop even if CAS fails
         }
     }
+}
+
+public fun rateLimiter(@BuilderInference block: RateLimiterBuilder.() -> Unit): RateLimiter {
+    val builder = RateLimiterBuilder()
+    builder.block()
+    return builder.build()
+}
+
+public class RateLimiterBuilder {
+
+    private val bandwidths = mutableListOf<Bandwidth>()
+
+    public fun smooth(capacity: Long, refillPeriod: Long) {
+        bandwidths.add(Bandwidth(capacity, refillPeriod / capacity))
+    }
+
+    @ExperimentalTime
+    public fun smooth(capacity: Long, refillPeriod: Duration) {
+        smooth(capacity, refillPeriod.toLongNanoseconds())
+    }
+
+    public fun periodic(capacity: Long, refillPeriod: Long, quantum: Long = 1, initialAmount: Long = capacity) {
+        bandwidths.add(Bandwidth(capacity, refillPeriod, quantum, initialAmount))
+    }
+
+    @ExperimentalTime
+    public fun periodic(capacity: Long, refillPeriod: Duration, quantum: Long = 1, initialAmount: Long = capacity) {
+        periodic(capacity, refillPeriod.toLongNanoseconds(), quantum, initialAmount)
+    }
+
+    internal fun build(): RateLimiter = TokenBucketRateLimiter(bandwidths.toTypedArray())
 }
