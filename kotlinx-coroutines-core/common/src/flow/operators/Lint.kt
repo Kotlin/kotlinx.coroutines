86925bb9cc47779e3b8750da5390e9eb7d6299c0
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-05-19 12:28:54 +0300

    Lint settings for 1.4
    The latest part of #2026

--- kotlinx-coroutines-core/common/src/flow/operators/Lint.kt
+++ kotlinx-coroutines-core/common/src/flow/operators/Lint.kt
@@ -2,6 +2,8 @@
  * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
+@file:Suppress("unused")
+
 package kotlinx.coroutines.flow
 
 import kotlinx.coroutines.*
@@ -43,30 +45,30 @@ public fun <T> StateFlow<T>.conflate(): Flow<T> = noImpl()
 )
 public fun <T> StateFlow<T>.distinctUntilChanged(): Flow<T> = noImpl()
 
-//@Deprecated(
-//    message = "isActive is resolved into the extension of outer CoroutineScope which is likely to be an error." +
-//        "Use currentCoroutineContext().isActive or cancellable() operator instead " +
-//        "or specify the receiver of isActive explicitly. " +
-//        "Additionally, flow {} builder emissions are cancellable by default.",
-//    level = DeprecationLevel.WARNING, // ERROR in 1.4
-//    replaceWith = ReplaceWith("currentCoroutineContext().isActive")
-//)
-//public val FlowCollector<*>.isActive: Boolean
-//    get() = noImpl()
-//
-//@Deprecated(
-//    message = "cancel() is resolved into the extension of outer CoroutineScope which is likely to be an error." +
-//        "Use currentCoroutineContext().cancel() instead or specify the receiver of cancel() explicitly",
-//    level = DeprecationLevel.WARNING, // ERROR in 1.4
-//    replaceWith = ReplaceWith("currentCoroutineContext().cancel(cause)")
-//)
-//public fun FlowCollector<*>.cancel(cause: CancellationException? = null): Unit = noImpl()
-//
-//@Deprecated(
-//    message = "coroutineContext is resolved into the property of outer CoroutineScope which is likely to be an error." +
-//        "Use currentCoroutineContext() instead or specify the receiver of coroutineContext explicitly",
-//    level = DeprecationLevel.WARNING, // ERROR in 1.4
-//    replaceWith = ReplaceWith("currentCoroutineContext()")
-//)
-//public val FlowCollector<*>.coroutineContext: CoroutineContext
-//    get() = noImpl()
+@Deprecated(
+    message = "isActive is resolved into the extension of outer CoroutineScope which is likely to be an error." +
+        "Use currentCoroutineContext().isActive or cancellable() operator instead " +
+        "or specify the receiver of isActive explicitly. " +
+        "Additionally, flow {} builder emissions are cancellable by default.",
+    level = DeprecationLevel.ERROR,
+    replaceWith = ReplaceWith("currentCoroutineContext().isActive")
+)
+public val FlowCollector<*>.isActive: Boolean
+    get() = noImpl()
+
+@Deprecated(
+    message = "cancel() is resolved into the extension of outer CoroutineScope which is likely to be an error." +
+        "Use currentCoroutineContext().cancel() instead or specify the receiver of cancel() explicitly",
+    level = DeprecationLevel.ERROR,
+    replaceWith = ReplaceWith("currentCoroutineContext().cancel(cause)")
+)
+public fun FlowCollector<*>.cancel(cause: CancellationException? = null): Unit = noImpl()
+
+@Deprecated(
+    message = "coroutineContext is resolved into the property of outer CoroutineScope which is likely to be an error." +
+        "Use currentCoroutineContext() instead or specify the receiver of coroutineContext explicitly",
+    level = DeprecationLevel.ERROR,
+    replaceWith = ReplaceWith("currentCoroutineContext()")
+)
+public val FlowCollector<*>.coroutineContext: CoroutineContext
+    get() = noImpl()
