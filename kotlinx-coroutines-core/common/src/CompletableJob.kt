82c87cfee92ad192764196484aa8446420a142a9
Roman Elizarov <elizarov@gmail.com>
2020-05-19 11:02:44 +0300

    Improved Job docs
    * A more consistent prose in the Job interface documentation, gradually explaining concepts without repetition.
    * Clear introduction and naming of "coroutine job" vs "completable job".
    * Explanation of what is a "cancellation cause" of Job and how that relates to CancellationException.
    * Added a note to all Job-derived interfaces that they are not safe for inheritance in 3rd party libraries.
    * Consistently copied a note on thread-safety to all job-derived interfaces.

--- kotlinx-coroutines-core/common/src/CompletableJob.kt
+++ kotlinx-coroutines-core/common/src/CompletableJob.kt
@@ -7,6 +7,12 @@ package kotlinx.coroutines
 /**
  * A job that can be completed using [complete()] function.
  * It is returned by [Job()][Job] and [SupervisorJob()][SupervisorJob] constructor functions.
+ *
+ * All functions on this interface are **thread-safe** and can
+ * be safely invoked from concurrent coroutines without external synchronization.
+ *
+ * **`CompletableJob` interface is not stable for inheritance in 3rd party libraries**,
+ * as new methods might be added to this interface in the future, but is stable for use.
  */
 public interface CompletableJob : Job {
     /**
