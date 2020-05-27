adbbbaa754fc6d64bc33a274c1638992c53a81f4
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-05-27 11:57:21 +0300

    Properly check identity of caught AbortFlowException in Flow.first op… (#2057)
    It fixes two problems:
        * NoSuchElementException can be thrown during cancellation sequence (see FirstJvmTest that reproduces this problem with explanation)
        * Cancellation can be accidentally suppressed and flow activity can be prolonged
    
    Fixes #2051

--- kotlinx-coroutines-core/common/src/flow/terminal/Reduce.kt
+++ kotlinx-coroutines-core/common/src/flow/terminal/Reduce.kt
@@ -8,9 +8,7 @@
 
 package kotlinx.coroutines.flow
 
-import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.internal.*
-import kotlinx.coroutines.flow.internal.unsafeFlow as flow
 import kotlin.jvm.*
 
 /**
@@ -84,15 +82,10 @@ public suspend fun <T: Any> Flow<T>.singleOrNull(): T? {
  */
 public suspend fun <T> Flow<T>.first(): T {
     var result: Any? = NULL
-    try {
-        collect { value ->
-            result = value
-            throw AbortFlowException(NopCollector)
-        }
-    } catch (e: AbortFlowException) {
-        // Do nothing
+    collectUntil {
+        result = it
+        true
     }
-
     if (result === NULL) throw NoSuchElementException("Expected at least one element")
     return result as T
 }
@@ -103,17 +96,14 @@ public suspend fun <T> Flow<T>.first(): T {
  */
 public suspend fun <T> Flow<T>.first(predicate: suspend (T) -> Boolean): T {
     var result: Any? = NULL
-    try {
-        collect { value ->
-            if (predicate(value)) {
-                result = value
-                throw AbortFlowException(NopCollector)
-            }
+    collectUntil {
+        if (predicate(it)) {
+            result = it
+            true
+        } else {
+            false
         }
-    } catch (e: AbortFlowException) {
-        // Do nothing
     }
-
     if (result === NULL) throw NoSuchElementException("Expected at least one element matching the predicate $predicate")
     return result as T
 }
@@ -124,13 +114,9 @@ public suspend fun <T> Flow<T>.first(predicate: suspend (T) -> Boolean): T {
  */
 public suspend fun <T : Any> Flow<T>.firstOrNull(): T? {
     var result: T? = null
-    try {
-        collect { value ->
-            result = value
-            throw AbortFlowException(NopCollector)
-        }
-    } catch (e: AbortFlowException) {
-        // Do nothing
+    collectUntil {
+        result = it
+        true
     }
     return result
 }
@@ -141,15 +127,28 @@ public suspend fun <T : Any> Flow<T>.firstOrNull(): T? {
  */
 public suspend fun <T : Any> Flow<T>.firstOrNull(predicate: suspend (T) -> Boolean): T? {
     var result: T? = null
-    try {
-        collect { value ->
-            if (predicate(value)) {
-                result = value
-                throw AbortFlowException(NopCollector)
-            }
+    collectUntil {
+        if (predicate(it)) {
+            result = it
+            true
+        } else {
+            false
         }
-    } catch (e: AbortFlowException) {
-        // Do nothing
     }
     return result
+}
+
+internal suspend inline fun <T> Flow<T>.collectUntil(crossinline block: suspend (value: T) -> Boolean) {
+    val collector = object : FlowCollector<T> {
+        override suspend fun emit(value: T) {
+            if (block(value)) {
+                throw AbortFlowException(this)
+            }
+        }
+    }
+    try {
+        collect(collector)
+    } catch (e: AbortFlowException) {
+        e.checkOwnership(collector)
+    }
 }
