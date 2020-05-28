82e422a00d94485aed8a2c0448915566532d7459
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2020-05-28 16:51:52 +0300

    Translate ConcurrentLinkedList to heaplang

---
+++ formal-proofs/theories/lib/util/addAndGet.v
@@ -0,0 +1,40 @@
+From iris.heap_lang Require Import notation lang.
+
+Section impl.
+
+Definition addAndGet: val :=
+  rec: "addAndGet" "loc" "delta" :=
+    let: "cur" := !"loc" in
+    let: "new" := "cur" + "delta" in
+    if: CAS "loc" "cur" "new"
+    then "new"
+    else "addAndGet" "loc" "delta".
+
+End impl.
+
+From iris.program_logic Require Import atomic.
+From iris.heap_lang Require Import proofmode.
+
+Section proof.
+
+Context `{heapG}.
+
+Theorem addAndGet_spec (ℓ: loc) (Δ: Z):
+  ⊢ <<< ∀ (k: Z), ▷ ℓ ↦ #k >>>
+    addAndGet #ℓ #Δ @ ⊤
+  <<< ℓ ↦ #(k + Δ), RET #(k + Δ) >>>.
+Proof.
+  iIntros (Φ) "AU". iLöb as "IH". wp_lam. wp_pures.
+  wp_bind (!_)%E. iMod "AU" as (k) "[Hℓ [HClose _]]".
+  wp_load. iMod ("HClose" with "Hℓ") as "AU". iModIntro. wp_pures.
+  wp_bind (CmpXchg _ _ _). iMod "AU" as (k') "[Hℓ HClose]".
+  destruct (decide (k = k')) as [<-|HContra].
+  - wp_cmpxchg_suc. iDestruct "HClose" as "[_ HClose]".
+    iMod ("HClose" with "Hℓ") as "HΦ".
+    iModIntro. wp_pures. iAssumption.
+  - wp_cmpxchg_fail. iDestruct "HClose" as "[HClose _]".
+    iMod ("HClose" with "Hℓ") as "AU".
+    iModIntro. wp_pures. iApply ("IH" with "AU").
+Qed.
+
+End proof.
---
+++ formal-proofs/theories/lib/util/addConditionally.v
@@ -0,0 +1,54 @@
+From iris.heap_lang Require Import notation lang.
+
+Definition addConditionally: val :=
+  rec: "addConditionally" "loc" "delta" "condition" :=
+    let: "cur" := !"loc" in
+    if: "condition" "cur"
+    then if: CAS "loc" "cur" ("cur" + "delta")
+         then #true
+         else "addConditionally" "loc" "delta" "condition"
+    else #false.
+
+From iris.program_logic Require Import atomic.
+From iris.heap_lang Require Import proofmode.
+
+Section proof.
+
+Context `{heapG}.
+
+Theorem addConditionally_spec (ℓ: loc) (Δ: Z) (condition: val) (pred: Z → bool):
+  ⊢ (∀ (k: Z), {{{ True }}}
+                 condition #k
+               {{{ RET #(pred k); True }}}) →
+    <<< ∀ (k: Z), ▷ ℓ ↦ #k >>>
+      addConditionally #ℓ #Δ condition @ ⊤
+    <<< ⌜pred k = false⌝ ∧ ℓ ↦ #k ∨
+        ⌜pred k = true⌝ ∧ ℓ ↦ #(k + Δ), RET #(pred k) >>>.
+Proof.
+  iIntros "#HCond" (Φ) "AU". iLöb as "IH". wp_lam. wp_pures.
+  wp_bind (!_)%E. iMod "AU" as (k) "[Hℓ HClose]".
+  wp_load.
+  destruct (pred k) eqn:E; wp_pures.
+  - iDestruct "HClose" as "[HClose _]".
+    iMod ("HClose" with "Hℓ") as "AU". iModIntro. wp_pures.
+    wp_apply ("HCond" with "[$]"). iIntros (_).
+    rewrite E. wp_pures.
+    wp_bind (CmpXchg _ _ _). iMod "AU" as (k') "[Hℓ HClose]".
+    destruct (decide (k = k')) as [<-|HContra].
+    + wp_cmpxchg_suc. iDestruct "HClose" as "[_ HClose]".
+      iMod ("HClose" with "[Hℓ]") as "HΦ".
+      { iRight. iFrame. by iPureIntro. }
+      iModIntro. wp_pures. rewrite E. iAssumption.
+    + wp_cmpxchg_fail. iDestruct "HClose" as "[HClose _]".
+      iMod ("HClose" with "Hℓ") as "AU".
+      iModIntro. wp_pures. iApply ("IH" with "AU").
+  - iDestruct "HClose" as "[_ HClose]".
+    iMod ("HClose" with "[Hℓ]") as "AU".
+    { iLeft. iFrame. by iPureIntro. }
+    iModIntro. wp_pures.
+    wp_apply ("HCond" with "[$]"). iIntros (_).
+    rewrite E. wp_pures.
+    iAssumption.
+Qed.
+
+End proof.
