e501881dbf7d1e83430a857302f804e05d602135
Roman Elizarov <elizarov@gmail.com>
2020-07-10 17:33:02 +0300

    ~ Fixup support of custom dispatcher names in Android
    Also remove hardcoded "Main" name in places of code where it is not needed anymore

--- ui/kotlinx-coroutines-android/test/HandlerDispatcherTest.kt
+++ ui/kotlinx-coroutines-android/test/HandlerDispatcherTest.kt
@@ -123,8 +123,8 @@ class HandlerDispatcherTest : TestBase() {
         ReflectionHelpers.setStaticField(Build.VERSION::class.java, "SDK_INT", 28)
         val main = Looper.getMainLooper().asHandler(async = true).asCoroutineDispatcher("testName")
         assertEquals("testName", main.toString())
-        assertEquals("testName [immediate]", main.immediate.toString())
-        assertEquals("testName [immediate]", main.immediate.immediate.toString())
+        assertEquals("testName.immediate", main.immediate.toString())
+        assertEquals("testName.immediate", main.immediate.immediate.toString())
     }
 
     private suspend fun Job.join(mainLooper: ShadowLooper) {
