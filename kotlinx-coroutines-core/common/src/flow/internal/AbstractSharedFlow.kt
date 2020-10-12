56ff4fb6ba6e3965d3f66bfd0634c1ab0099455b
Roman Elizarov <elizarov@gmail.com>
2020-10-12 18:21:27 +0300

    ~ Moved AbstractSharedFlow to internal package

--- kotlinx-coroutines-core/common/src/flow/internal/AbstractSharedFlow.kt
+++ kotlinx-coroutines-core/common/src/flow/internal/AbstractSharedFlow.kt
@@ -2,8 +2,9 @@
  * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.coroutines.flow
+package kotlinx.coroutines.flow.internal
 
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.internal.*
 import kotlin.coroutines.*
 import kotlin.jvm.*
