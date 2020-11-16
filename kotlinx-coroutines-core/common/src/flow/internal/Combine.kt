bc553ba7c5aeb3a7fddad23e47b2e11e9de8f297
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-11-16 11:23:41 -0800

    Conditionally create an instance of CancellationException in Channel.cancel() (#2384)
    Avoid creating costly exception when the channel is cancelled to save a few cycles when it's not necessary. Cancellation is heavy-enough when the channel is open, so the single check won't worsen it.

--- kotlinx-coroutines-core/common/src/flow/internal/Combine.kt
+++ kotlinx-coroutines-core/common/src/flow/internal/Combine.kt
@@ -137,7 +137,7 @@ internal fun <T1, T2, R> zipImpl(flow: Flow<T1>, flow2: Flow<T2>, transform: sus
             } catch (e: AbortFlowException) {
                 e.checkOwnership(owner = this@unsafeFlow)
             } finally {
-                if (!second.isClosedForReceive) second.cancel()
+                second.cancel()
             }
         }
     }
