dd5c9cabe04520495a34c92d4d298b8eb7b29cd5
Roman Elizarov <elizarov@gmail.com>
2020-12-04 21:27:42 +0300

    ~ a different approach that actually works on JS, simplifies code

--- kotlinx-coroutines-core/common/src/Job.kt
+++ kotlinx-coroutines-core/common/src/Job.kt
@@ -490,7 +490,7 @@ public interface ChildHandle : DisposableHandle {
  * ```
  */
 internal fun Job.disposeOnCompletion(handle: DisposableHandle): DisposableHandle =
-    invokeOnCompletion(handler = DisposeOnCompletion(this, handle).asHandler)
+    invokeOnCompletion(handler = DisposeOnCompletion(handle).asHandler)
 
 /**
  * Cancels the job and suspends the invoking coroutine until the cancelled job is complete.
