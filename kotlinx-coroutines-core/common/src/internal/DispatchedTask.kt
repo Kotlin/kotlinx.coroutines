3fa8ee681989227144551ea5ffa513c8319aa35d
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-02-02 17:17:07 +0300

    Properly nest ThreadContextElement
        * Restore the context in the reverse order of update, so they are properly nested into each other
        * Also, do a minor cleanup
    
    Fixes #2195

--- kotlinx-coroutines-core/common/src/internal/DispatchedTask.kt
+++ kotlinx-coroutines-core/common/src/internal/DispatchedTask.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package kotlinx.coroutines
@@ -134,7 +134,7 @@ internal abstract class DispatchedTask<in T>(
      * Fatal exception handling can be intercepted with [CoroutineExceptionHandler] element in the context of
      * a failed coroutine, but such exceptions should be reported anyway.
      */
-    internal fun handleFatalException(exception: Throwable?, finallyException: Throwable?) {
+    public fun handleFatalException(exception: Throwable?, finallyException: Throwable?) {
         if (exception === null && finallyException === null) return
         if (exception !== null && finallyException !== null) {
             exception.addSuppressedThrowable(finallyException)
