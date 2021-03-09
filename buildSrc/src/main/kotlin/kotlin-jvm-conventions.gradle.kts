6af3161dbc0972b0b6e68372b9abdae722b02a4b
Yahor Berdnikau <egorr.berd@gmail.com>
2021-03-09 11:26:14 +0100

    Replace deprecated '*compile' configurations.
    They were deprecated long time ago and were removed in Gradle 7.

--- buildSrc/src/main/kotlin/kotlin-jvm-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm-conventions.gradle.kts
@@ -22,12 +22,12 @@ if (rootProject.extra.get("jvm_ir_enabled") as Boolean) {
 }
 
 dependencies {
-    testCompile(kotlin("test"))
+    testImplementation(kotlin("test"))
     // Workaround to make addSuppressed work in tests
-    testCompile(kotlin("reflect"))
-    testCompile(kotlin("stdlib-jdk7"))
-    testCompile(kotlin("test-junit"))
-    testCompile("junit:junit:${version("junit")}")
+    testImplementation(kotlin("reflect"))
+    testImplementation(kotlin("stdlib-jdk7"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("junit:junit:${version("junit")}")
 }
 
 tasks.compileKotlin {
