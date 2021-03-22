6f13927ea3835f433ebfd309ed3767386ef420e7
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-03-22 15:09:30 +0300

    Do not initialize CoroutineExceptionHandler in Job <clinit> (#2513)
    * It slightly cuts down startup time
        * It simplifies the code in general
        * It do not serve its purpose, coroutines are already protected from StackOverflowError with nested event-loops where applicable

--- kotlinx-coroutines-core/common/src/Job.kt
+++ kotlinx-coroutines-core/common/src/Job.kt
@@ -113,16 +113,7 @@ public interface Job : CoroutineContext.Element {
     /**
      * Key for [Job] instance in the coroutine context.
      */
-    public companion object Key : CoroutineContext.Key<Job> {
-        init {
-            /*
-             * Here we make sure that CoroutineExceptionHandler is always initialized in advance, so
-             * that if a coroutine fails due to StackOverflowError we don't fail to report this error
-             * trying to initialize CoroutineExceptionHandler
-             */
-            CoroutineExceptionHandler
-        }
-    }
+    public companion object Key : CoroutineContext.Key<Job>
 
     // ------------ state query ------------
 
