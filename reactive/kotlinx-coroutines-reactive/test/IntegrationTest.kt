86a0658af37d149c4e3a866f9aebf1c537a48238
dkhalanskyjb <52952525+dkhalanskyjb@users.noreply.github.com>
2021-04-07 13:34:27 +0300

    Make the subscriber in awaitOne less permissive (#2586)
    The implementation of Reactive Streams' Subscriber used for
    `await*` operations was assuming that the publisher is correct.
    Now, the implementation detects some instances of problematic
    behavior for publishers and reports them.
    
    Fixes https://github.com/Kotlin/kotlinx.coroutines/issues/2079

--- reactive/kotlinx-coroutines-reactive/test/IntegrationTest.kt
+++ reactive/kotlinx-coroutines-reactive/test/IntegrationTest.kt
@@ -9,6 +9,8 @@ import org.junit.Test
 import org.junit.runner.*
 import org.junit.runners.*
 import org.reactivestreams.*
+import java.lang.IllegalStateException
+import java.lang.RuntimeException
 import kotlin.coroutines.*
 import kotlin.test.*
 
@@ -128,6 +130,136 @@ class IntegrationTest(
 
         job.join()
         finish(3)
+    }
+
+    /**
+     * Test that the continuation is not being resumed after it has already failed due to there having been too many
+     * values passed.
+     */
+    @Test
+    fun testNotCompletingFailedAwait() = runTest {
+        try {
+            expect(1)
+            Publisher<Int> { sub ->
+                sub.onSubscribe(object: Subscription {
+                    override fun request(n: Long) {
+                        expect(2)
+                        sub.onNext(1)
+                        sub.onNext(2)
+                        expect(4)
+                        sub.onComplete()
+                    }
+
+                    override fun cancel() {
+                        expect(3)
+                    }
+                })
+            }.awaitSingle()
+        } catch (e: java.lang.IllegalArgumentException) {
+            expect(5)
+        }
+        finish(6)
+    }
+
+    /**
+     * Test the behavior of [awaitOne] on unconforming publishers.
+     */
+    @Test
+    fun testAwaitOnNonconformingPublishers() = runTest {
+        fun <T> publisher(block: Subscriber<in T>.(n: Long) -> Unit) =
+            Publisher<T> { subscriber ->
+                subscriber.onSubscribe(object: Subscription {
+                    override fun request(n: Long) {
+                        subscriber.block(n)
+                    }
+
+                    override fun cancel() {
+                    }
+                })
+            }
+        val dummyMessage = "dummy"
+        val dummyThrowable = RuntimeException(dummyMessage)
+        suspend fun <T> assertDetectsBadPublisher(
+            operation: suspend Publisher<T>.() -> T,
+            message: String,
+            block: Subscriber<in T>.(n: Long) -> Unit,
+        ) {
+            assertCallsExceptionHandlerWith<IllegalStateException> {
+                try {
+                    publisher(block).operation()
+                } catch (e: Throwable) {
+                    if (e.message != dummyMessage)
+                        throw e
+                }
+            }.let {
+                assertTrue("Expected the message to contain '$message', got '${it.message}'") {
+                    it.message?.contains(message) ?: false
+                }
+            }
+        }
+
+        // Rule 1.1 broken: the publisher produces more values than requested.
+        assertDetectsBadPublisher<Int>({ awaitFirst() }, "provided more") {
+            onNext(1)
+            onNext(2)
+            onComplete()
+        }
+
+        // Rule 1.7 broken: the publisher calls a method on a subscriber after reaching the terminal state.
+        assertDetectsBadPublisher<Int>({ awaitSingle() }, "terminal state") {
+            onNext(1)
+            onError(dummyThrowable)
+            onComplete()
+        }
+        assertDetectsBadPublisher<Int>({ awaitSingleOrDefault(2) }, "terminal state") {
+            onComplete()
+            onError(dummyThrowable)
+        }
+        assertDetectsBadPublisher<Int>({ awaitFirst() }, "terminal state") {
+            onNext(0)
+            onComplete()
+            onComplete()
+        }
+        assertDetectsBadPublisher<Int>({ awaitFirstOrDefault(1) }, "terminal state") {
+            onComplete()
+            onNext(3)
+        }
+        assertDetectsBadPublisher<Int>({ awaitSingle() }, "terminal state") {
+            onError(dummyThrowable)
+            onNext(3)
+        }
+
+        // Rule 1.9 broken (the first signal to the subscriber was not 'onSubscribe')
+        assertCallsExceptionHandlerWith<IllegalStateException> {
+            try {
+                Publisher<Int> { subscriber ->
+                    subscriber.onNext(3)
+                    subscriber.onComplete()
+                }.awaitFirst()
+            } catch (e: NoSuchElementException) {
+                // intentionally blank
+            }
+        }.let { assertTrue(it.message?.contains("onSubscribe") ?: false) }
+    }
+
+    private suspend inline fun <reified E: Throwable> assertCallsExceptionHandlerWith(
+        crossinline operation: suspend () -> Unit): E
+    {
+        val caughtExceptions = mutableListOf<Throwable>()
+        val exceptionHandler = object: AbstractCoroutineContextElement(CoroutineExceptionHandler),
+            CoroutineExceptionHandler
+        {
+            override fun handleException(context: CoroutineContext, exception: Throwable) {
+                caughtExceptions += exception
+            }
+        }
+        return withContext(exceptionHandler) {
+            operation()
+            caughtExceptions.single().let {
+                assertTrue(it is E)
+                it
+            }
+        }
     }
 
     private suspend fun checkNumbers(n: Int, pub: Publisher<Int>) {
