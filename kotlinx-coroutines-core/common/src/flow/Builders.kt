e3f2b9feb230e3955568c880eabcdcae252f44cd
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-04-12 13:52:13 +0300

    Embrace new channel API
        * Introduce trySendBlocking and deprecate sendBlocking
        * Use it in callbackFlow example
        * Stabilize callbackFlow and channelFlow as they finally have error-safe API
        * Irrelevant: migrate from deprecated stdlib API to be able to build with Kotlin 1.5

--- kotlinx-coroutines-core/common/src/flow/Builders.kt
+++ kotlinx-coroutines-core/common/src/flow/Builders.kt
@@ -261,7 +261,6 @@ public fun <T> flowViaChannel(
  * }
  * ```
  */
-@ExperimentalCoroutinesApi
 public fun <T> channelFlow(@BuilderInference block: suspend ProducerScope<T>.() -> Unit): Flow<T> =
     ChannelFlowBuilder(block)
 
@@ -302,11 +301,10 @@ public fun <T> channelFlow(@BuilderInference block: suspend ProducerScope<T>.() 
  *         override fun onNextValue(value: T) {
  *             // To avoid blocking you can configure channel capacity using
  *             // either buffer(Channel.CONFLATED) or buffer(Channel.UNLIMITED) to avoid overfill
- *             try {
- *                 sendBlocking(value)
- *             } catch (e: Exception) {
- *                 // Handle exception from the channel: failure in flow or premature closing
- *             }
+ *             trySendBlocking(value)
+ *                 .onFailure { throwable ->
+ *                     // Downstream has been cancelled or failed, can log here
+ *                 }
  *         }
  *         override fun onApiError(cause: Throwable) {
  *             cancel(CancellationException("API Error", cause))
@@ -327,7 +325,6 @@ public fun <T> channelFlow(@BuilderInference block: suspend ProducerScope<T>.() 
  * > `awaitClose` block can be called at any time due to asynchronous nature of cancellation, even
  * > concurrently with the call of the callback.
  */
-@ExperimentalCoroutinesApi
 public fun <T> callbackFlow(@BuilderInference block: suspend ProducerScope<T>.() -> Unit): Flow<T> = CallbackFlowBuilder(block)
 
 // ChannelFlow implementation that is the first in the chain of flow operations and introduces (builds) a flow
--- kotlinx-coroutines-core/jvm/src/channels/Channels.kt
+++ kotlinx-coroutines-core/jvm/src/channels/Channels.kt
@@ -10,12 +10,25 @@ package kotlinx.coroutines.channels
 import kotlinx.coroutines.*
 
 /**
- * Adds [element] into to this channel, **blocking** the caller while this channel is full,
- * or throws exception if the channel [Channel.isClosedForSend] (see [Channel.close] for details).
+ * ### Deprecation note.
  *
- * This is a way to call [Channel.send] method inside a blocking code using [runBlocking],
- * so this function should not be used from coroutine.
+ * This method is deprecated in the favour of [trySendBlocking].
+ *
+ * `sendBlocking` is considered to be dangerous primitive -- it throws
+ * if the channel was closed or, more commonly, cancelled.
+ * Cancellation exceptions are not ignored by non-blocking code and frequently
+ * trigger internal failures.
+ *
+ * These bugs were hard-to-spot during code review and also forced users to write
+ * their own wrappers around `sendBlocking`, so it was decided to deprecate
+ * this function and provide a more explicit primitive instead.
  */
+@Deprecated(
+    level = DeprecationLevel.WARNING,
+    message = "Deprecated in the favour of 'trySendBlocking'. " +
+        "Consider handle the result of 'trySendBlocking' explicitly and rethrow exception if necessary",
+    replaceWith = ReplaceWith("trySendBlocking(element)")
+)
 public fun <E> SendChannel<E>.sendBlocking(element: E) {
     // fast path
     if (offer(element))
@@ -23,5 +36,40 @@ public fun <E> SendChannel<E>.sendBlocking(element: E) {
     // slow path
     runBlocking {
         send(element)
+    }
+}
+
+/**
+ * Adds [element] into to this channel, **blocking** the caller while this channel is full,
+ * and returning either [successful][ChannelResult.isSuccess] result when the element was added, or
+ * failed result representing closed channel with a corresponding exception.
+ *
+ * This is a way to call [Channel.send] method in a safe manner inside a blocking code using [runBlocking] and catching,
+ * so this function should not be used from coroutine.
+ *
+ * Example of usage:
+ * ```
+ * // From callback API
+ * channel.trySendBlocking(element)
+ *     .onSuccess { /* request next element or debug log */ }
+ *     .onFailure { t: Throwable? -> /* throw or log */ }
+ *
+ * ```
+ * For this operation it is guaranteed that [failure][ChannelResult.failed] always contains an exception in it.
+ *
+ * @throws [InterruptedException] if the current thread is interrupted during the blocking send operation.
+ */
+public fun <E> SendChannel<E>.trySendBlocking(element: E): ChannelResult<Unit> {
+    /*
+     * Sent successfully -- bail out.
+     * But failure may indicate either that the channel it full or that
+     * it is close. Go to slow path on failure to simplify the successful path and
+     * to materialize default exception.
+     */
+    trySend(element).onSuccess { return ChannelResult.success(Unit) }
+    return runBlocking {
+        val r = runCatching { send(element) }
+        if (r.isSuccess) ChannelResult.success(Unit)
+        else ChannelResult.closed(r.exceptionOrNull())
     }
 }
