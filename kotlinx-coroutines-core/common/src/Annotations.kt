7ef1f1598a754cf2f3091c99c0be8cd8469bd41c
Roman Elizarov <elizarov@gmail.com>
2021-04-15 17:30:30 +0300

    ~ Message for delicate API opt-in

--- kotlinx-coroutines-core/common/src/Annotations.kt
+++ kotlinx-coroutines-core/common/src/Annotations.kt
@@ -15,7 +15,11 @@ import kotlinx.coroutines.flow.*
  */
 @MustBeDocumented
 @Retention(value = AnnotationRetention.BINARY)
-@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
+@RequiresOptIn(
+    level = RequiresOptIn.Level.WARNING,
+    message = "This is a delicate API and its use requires care." +
+        " Make sure you fully read and understand documentation of the declaration that is marked as a delicate API."
+)
 public annotation class DelicateCoroutinesApi
 
 /**
