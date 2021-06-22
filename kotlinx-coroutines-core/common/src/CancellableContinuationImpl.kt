2e4f042587f5b3f09c59a5f554287728f22f934c
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-06-22 20:10:28 +0300

    Better check whether the CC is reusable
        * Ensure that the caller to DispatchedContinuation.isReusable is reusable itself
        * Using previous invariant, simplify DispatchedContinuation.isReusable to a single null-check
        * It also restores the invariant that `cc.isReusable() == cc.resumeMode.isReusableMode`

--- kotlinx-coroutines-core/common/src/CancellableContinuationImpl.kt
+++ kotlinx-coroutines-core/common/src/CancellableContinuationImpl.kt
@@ -91,18 +91,6 @@ internal open class CancellableContinuationImpl<in T>(
 
     public override fun initCancellability() {
         /*
-         * We could've been in the following situation:
-         * suspendCancellableCoroutineReusable {} // <- CLAIMED, trySuspend(), now blocked by OS schedulder
-         * suspendCancellableCoroutine {} // <- we are here
-         *
-         * In such rare cases, we explicitly await for reusable continuation to became available and
-         * for `isReusable` to start returning `false`.
-         */
-        if (isReusable()) {
-            (delegate as DispatchedContinuation<*>).awaitReusability()
-        }
-
-        /*
         * Invariant: at the moment of invocation, `this` has not yet
         * leaked to user code and no one is able to invoke `resume` or `cancel`
         * on it yet. Also, this function is not invoked for reusable continuations.
@@ -119,7 +107,7 @@ internal open class CancellableContinuationImpl<in T>(
         }
     }
 
-    private fun isReusable(): Boolean = delegate is DispatchedContinuation<*> && delegate.isReusable(this)
+    private fun isReusable(): Boolean = resumeMode.isReusableMode && (delegate as DispatchedContinuation<*>).isReusable()
 
     /**
      * Resets cancellability state in order to [suspendCancellableCoroutineReusable] to work.
@@ -127,7 +115,7 @@ internal open class CancellableContinuationImpl<in T>(
      */
     @JvmName("resetStateReusable") // Prettier stack traces
     internal fun resetStateReusable(): Boolean {
-        assert { resumeMode == MODE_CANCELLABLE_REUSABLE } // invalid mode for CancellableContinuationImpl
+        assert { resumeMode == MODE_CANCELLABLE_REUSABLE }
         assert { parentHandle !== NonDisposableHandle }
         val state = _state.value
         assert { state !is NotCompleted }
@@ -176,8 +164,7 @@ internal open class CancellableContinuationImpl<in T>(
      * Attempt to postpone cancellation for reusable cancellable continuation
      */
     private fun cancelLater(cause: Throwable): Boolean {
-        if (!resumeMode.isReusableMode) return false
-        // Ensure that we are postponing cancellation to the right instance
+        // Ensure that we are postponing cancellation to the right reusable instance
         if (!isReusable()) return false
         val dispatched = delegate as DispatchedContinuation<*>
         return dispatched.postponeCancellation(cause)
