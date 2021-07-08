46fa5d072e194257899b7fd65eae0286bbd013bb
mvicsokolova <maria.sokolova@jetbrains.com>
2021-07-08 14:29:09 +0300

    Review fixes

--- kotlinx-coroutines-core/native/src/CoroutineExceptionHandlerImpl.kt
+++ kotlinx-coroutines-core/native/src/CoroutineExceptionHandlerImpl.kt
@@ -1,6 +1,7 @@
 /*
  * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+@file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
 
 package kotlinx.coroutines
 
@@ -8,11 +9,13 @@ import kotlin.coroutines.*
 import kotlin.system.exitProcess
 import kotlin.native.*
 import kotlin.native.concurrent.*
+import kotlin.native.internal.OnUnhandledException
 
 internal actual fun handleCoroutineExceptionImpl(context: CoroutineContext, exception: Throwable) {
-    // Use unhandled exception hook if it is set, otherwise print the stacktrace
-    val hook = setUnhandledExceptionHook({ _: Throwable -> }.freeze())
-    if (hook != null) hook(exception) else exception.printStackTrace()
+    val throwable = Throwable(
+        "The process was terminated due to the unhandled exception thrown in the coroutine $context: ${exception.message}",
+        exception.cause)
+    OnUnhandledException(throwable)
     // Terminate the process
     exitProcess(-1)
 }
