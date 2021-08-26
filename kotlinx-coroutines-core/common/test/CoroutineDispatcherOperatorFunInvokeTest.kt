dfa1b6342e4cfb5316f87cb6fc3145d172f9b436
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-08-26 13:33:09 +0300

    Update Kotlin (#2902)
    * Update Kotlin to 1.5.30
    * Add new M1 targets

--- kotlinx-coroutines-core/common/test/CoroutineDispatcherOperatorFunInvokeTest.kt
+++ kotlinx-coroutines-core/common/test/CoroutineDispatcherOperatorFunInvokeTest.kt
@@ -65,7 +65,6 @@ class CoroutineDispatcherOperatorFunInvokeTest : TestBase() {
             dispatcher.dispatch(context, block)
         }
 
-        @ExperimentalCoroutinesApi
         override fun isDispatchNeeded(context: CoroutineContext): Boolean {
             return dispatcher.isDispatchNeeded(context)
         }
