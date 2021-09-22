4d9aad75b50e8a1b45894d13d7405c9506b96a75
anastasiia.spaseeva <anastasiiia.spaseeva@jetbrains.com>
2021-09-22 12:31:38 +0200

    Revert "WA: comment kotlinx.coroutines/kotlinx-coroutines-core/jvm/test/guide/example-channel-10.kt"
    This reverts commit 5427d010d3f78e99dadaea7581c79bde2cb4ad61.

--- kotlinx-coroutines-core/jvm/test/guide/example-channel-10.kt
+++ kotlinx-coroutines-core/jvm/test/guide/example-channel-10.kt
@@ -1,33 +1,33 @@
-///*
-// * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
-// */
-//
-//// This file was automatically generated from channels.md by Knit tool. Do not edit.
-//package kotlinx.coroutines.guide.exampleChannel10
-//
-//import kotlinx.coroutines.*
-//import kotlinx.coroutines.channels.*
-//
-//fun main() = runBlocking<Unit> {
-//    val tickerChannel = ticker(delayMillis = 100, initialDelayMillis = 0) // create ticker channel
-//    var nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
-//    println("Initial element is available immediately: $nextElement") // no initial delay
-//
-//    nextElement = withTimeoutOrNull(50) { tickerChannel.receive() } // all subsequent elements have 100ms delay
-//    println("Next element is not ready in 50 ms: $nextElement")
-//
-//    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
-//    println("Next element is ready in 100 ms: $nextElement")
-//
-//    // Emulate large consumption delays
-//    println("Consumer pauses for 150ms")
-//    delay(150)
-//    // Next element is available immediately
-//    nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
-//    println("Next element is available immediately after large consumer delay: $nextElement")
-//    // Note that the pause between `receive` calls is taken into account and next element arrives faster
-//    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
-//    println("Next element is ready in 50ms after consumer pause in 150ms: $nextElement")
-//
-//    tickerChannel.cancel() // indicate that no more elements are needed
-//}
+/*
+ * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+// This file was automatically generated from channels.md by Knit tool. Do not edit.
+package kotlinx.coroutines.guide.exampleChannel10
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+
+fun main() = runBlocking<Unit> {
+    val tickerChannel = ticker(delayMillis = 100, initialDelayMillis = 0) // create ticker channel
+    var nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
+    println("Initial element is available immediately: $nextElement") // no initial delay
+
+    nextElement = withTimeoutOrNull(50) { tickerChannel.receive() } // all subsequent elements have 100ms delay
+    println("Next element is not ready in 50 ms: $nextElement")
+
+    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
+    println("Next element is ready in 100 ms: $nextElement")
+
+    // Emulate large consumption delays
+    println("Consumer pauses for 150ms")
+    delay(150)
+    // Next element is available immediately
+    nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
+    println("Next element is available immediately after large consumer delay: $nextElement")
+    // Note that the pause between `receive` calls is taken into account and next element arrives faster
+    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() } 
+    println("Next element is ready in 50ms after consumer pause in 150ms: $nextElement")
+
+    tickerChannel.cancel() // indicate that no more elements are needed
+}
