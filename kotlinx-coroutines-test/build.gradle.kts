5520b65d266ba2b0245ddbe07d545156f98fb325
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2021-10-01 16:33:39 +0300

    Remove unused experimental annotations from the test module

--- kotlinx-coroutines-test/build.gradle.kts
+++ kotlinx-coroutines-test/build.gradle.kts
@@ -1,16 +1,10 @@
 /*
  * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
-import java.net.URL
 
 val experimentalAnnotations = listOf(
-    "kotlin.Experimental",
-    "kotlin.experimental.ExperimentalTypeInference",
-    "kotlin.ExperimentalMultiplatform",
     "kotlinx.coroutines.ExperimentalCoroutinesApi",
-    "kotlinx.coroutines.ObsoleteCoroutinesApi",
-    "kotlinx.coroutines.InternalCoroutinesApi",
-    "kotlinx.coroutines.FlowPreview"
+    "kotlinx.coroutines.InternalCoroutinesApi"
 )
 
 kotlin {
@@ -24,7 +18,6 @@ kotlin {
             resources.srcDir("$platform/test-resources")
         }
         languageSettings {
-            progressiveMode = true
             experimentalAnnotations.forEach { useExperimentalAnnotation(it) }
         }
     }
