ebaddcd783dad415d9a815c3ba2d69ff9fa42213
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2021-10-14 21:35:28 +0300

    Add more tests for 'runTest'

---
+++ kotlinx-coroutines-test/jvm/test/Helpers.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+package kotlinx.coroutines.test
+
+import kotlinx.coroutines.*
+
+actual fun testResultMap(block: (() -> Unit) -> Unit, test: () -> TestResult) {
+    block {
+        test()
+    }
+}
