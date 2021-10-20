f67648479b0c64f4fe7fad41650c87d4d139e72a
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-10-20 12:05:58 +0300

    ~license

--- buildSrc/src/main/kotlin/bom-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/bom-conventions.gradle.kts
@@ -1,9 +1,9 @@
-import org.gradle.kotlin.dsl.*
-import org.jetbrains.kotlin.gradle.dsl.*
-
 /*
  * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.gradle.dsl.*
+
 
 configure(subprojects.filter { it.name !in unpublished }) {
     if (name == "kotlinx-coroutines-bom" || name == "kotlinx.coroutines") return@configure
