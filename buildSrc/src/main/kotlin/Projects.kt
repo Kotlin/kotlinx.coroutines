e6592c4e3a69a2cbf65795c533ec77cde3814e2f
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-10-20 12:06:22 +0300

    Introduce coroutines platform to have all the dependencies aligned (#2952)
    * Introduce coroutines platform to have all the dependencies aligned
    
    Fixes #2865

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -1,8 +1,20 @@
 /*
  * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
-
+@file:JvmName("Projects")
 import org.gradle.api.Project
 
 fun Project.version(target: String): String =
     property("${target}_version") as String
+
+val coreModule = "kotlinx-coroutines-core"
+val testModule = "kotlinx-coroutines-test"
+
+val multiplatform = setOf(coreModule, testModule)
+// Not applicable for Kotlin plugin
+val sourceless = setOf("kotlinx.coroutines", "kotlinx-coroutines-bom", "integration-testing")
+val internal = setOf("kotlinx.coroutines", "benchmarks", "integration-testing")
+// Not published
+val unpublished = internal + setOf("example-frontend-js", "android-unit-tests")
+
+val Project.isMultiplatform: Boolean get() = name in multiplatform
