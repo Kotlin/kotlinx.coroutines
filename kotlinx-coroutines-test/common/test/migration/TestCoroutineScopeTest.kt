c014717a7c20050ff1d94dcaf9b25b578fd56371
dkhalanskyjb <52952525+dkhalanskyjb@users.noreply.github.com>
2021-11-16 15:59:09 +0300

    Implement TestScope (#3015)
    Co-authored-by: Vsevolod Tolstopyatov <qwwdfsad@gmail.com>

--- kotlinx-coroutines-test/common/test/migration/TestCoroutineScopeTest.kt
+++ kotlinx-coroutines-test/common/test/migration/TestCoroutineScopeTest.kt
@@ -1,6 +1,7 @@
 /*
- * Copyright 2016-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+@file:Suppress("DEPRECATION")
 
 package kotlinx.coroutines.test
 
