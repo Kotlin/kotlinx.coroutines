f940257b5b518fbcd5ce3dc583970d56f89b6f11
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2022-06-02 13:37:36 +0300

    Update withContext KDoc
    Fixes #3167

--- kotlinx-coroutines-core/common/src/Builders.common.kt
+++ kotlinx-coroutines-core/common/src/Builders.common.kt
@@ -126,12 +126,13 @@ private class LazyDeferredCoroutine<T>(
  * This suspending function is cancellable. It immediately checks for cancellation of
  * the resulting context and throws [CancellationException] if it is not [active][CoroutineContext.isActive].
  *
- * This function uses dispatcher from the new context, shifting execution of the [block] into the
- * different thread if a new dispatcher is specified, and back to the original dispatcher
- * when it completes. Note that the result of `withContext` invocation is
- * dispatched into the original context in a cancellable way with a **prompt cancellation guarantee**,
- * which means that if the original [coroutineContext], in which `withContext` was invoked,
- * is cancelled by the time its dispatcher starts to execute the code,
+ * This function redispatches the given [block] if and only if a different [CoroutineDispatcher]
+ * has been explicitly supplied with `coroutineContext` argument, shifting the execution
+ * back to an original dispatcher when the [block] completes.
+ *
+ * Note that the result of `withContext` invocation is dispatched into the original context in a cancellable way
+ * with a **prompt cancellation guarantee**, which means that if the original [coroutineContext],
+ * in which `withContext` was invoked, is cancelled by the time its dispatcher starts to execute the code,
  * it discards the result of `withContext` and throws [CancellationException].
  *
  * The cancellation behaviour described above is enabled if and only if the dispatcher is being changed.
