01d86142fd33cdeec17e4c2100ad7810881f3b68
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2022-06-17 11:55:45 +0200

    A prototype of ThreadContextElement on k/js and k/native (copy/paste from jvm sources)

--- kotlinx-coroutines-core/js/src/internal/ThreadContext.kt
+++ kotlinx-coroutines-core/js/src/internal/ThreadContext.kt
@@ -6,4 +6,4 @@ package kotlinx.coroutines.internal
 
 import kotlin.coroutines.*
 
-internal actual fun threadContextElements(context: CoroutineContext): Any = 0
+//internal actual fun threadContextElements(context: CoroutineContext): Any = 0
