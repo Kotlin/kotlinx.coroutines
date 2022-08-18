2ea090f008c374656286b6cb31fafc3480f7737f
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2022-08-18 15:41:00 +0200

    Properly use UndispatchedCoroutine context for context-specific elements
    Fixes #3411

--- kotlinx-coroutines-core/jvm/test/ThreadContextElementTest.kt
+++ kotlinx-coroutines-core/jvm/test/ThreadContextElementTest.kt
@@ -156,6 +156,41 @@ class ThreadContextElementTest : TestBase() {
             }
         }
     }
+
+    class JobCaptor(val capturees: ArrayList<Job> = ArrayList()) : ThreadContextElement<Unit> {
+
+        companion object Key : CoroutineContext.Key<MyElement>
+
+        override val key: CoroutineContext.Key<*> get() = Key
+
+        override fun updateThreadContext(context: CoroutineContext) {
+            capturees.add(context.job)
+        }
+
+        override fun restoreThreadContext(context: CoroutineContext, oldState: Unit) {
+        }
+    }
+
+    @Test
+    fun testWithContextJobAccess() = runTest {
+        val captor = JobCaptor()
+        val manuallyCaptured = ArrayList<Job>()
+        runBlocking(captor) {
+            manuallyCaptured += coroutineContext.job
+            withContext(CoroutineName("undispatched")) {
+                manuallyCaptured += coroutineContext.job
+                withContext(Dispatchers.IO) {
+                    manuallyCaptured += coroutineContext.job
+                }
+                // Context restored, captured again
+                manuallyCaptured += coroutineContext.job
+            }
+            // Context restored, captured again
+            manuallyCaptured += coroutineContext.job
+        }
+
+        assertEquals(manuallyCaptured, captor.capturees)
+    }
 }
 
 class MyData
