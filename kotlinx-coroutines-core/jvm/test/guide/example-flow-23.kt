4f686c8c6ccdb2a535247479bc406da1679598be
gw111zz <gw111zz@gmail.com>
2022-09-20 12:26:54 +0200

    Grammar fixes to flow.md (#3382)

--- kotlinx-coroutines-core/jvm/test/guide/example-flow-23.kt
+++ kotlinx-coroutines-core/jvm/test/guide/example-flow-23.kt
@@ -16,7 +16,7 @@ fun requestFlow(i: Int): Flow<String> = flow {
 
 fun main() = runBlocking<Unit> { 
     val startTime = currentTimeMillis() // remember the start time 
-    (1..3).asFlow().onEach { delay(100) } // a number every 100 ms 
+    (1..3).asFlow().onEach { delay(100) } // emit a number every 100 ms 
         .flatMapConcat { requestFlow(it) }                                                                           
         .collect { value -> // collect and print 
             println("$value at ${currentTimeMillis() - startTime} ms from start") 
