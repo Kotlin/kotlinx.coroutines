028591412e9deb80c7f65704ce794218b89169f7
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2023-02-17 15:50:14 +0100

    Assert Java 8 compliance in PrecompiledDebugProbesTest after migration to Java 8

--- integration-testing/src/debugAgentTest/kotlin/PrecompiledDebugProbesTest.kt
+++ integration-testing/src/debugAgentTest/kotlin/PrecompiledDebugProbesTest.kt
@@ -18,7 +18,7 @@ class PrecompiledDebugProbesTest {
         val clz = Class.forName("kotlin.coroutines.jvm.internal.DebugProbesKt")
         val classFileResourcePath = clz.name.replace(".", "/") + ".class"
         val array = clz.classLoader.getResourceAsStream(classFileResourcePath).use { it.readBytes() }
-        assertJava6Compliance(array)
+        assertJava8Compliance(array)
         // we expect the integration testing project to be in a subdirectory of the main kotlinx.coroutines project
         val base = File("").absoluteFile.parentFile
         val probes = File(base, "kotlinx-coroutines-core/jvm/resources/DebugProbesKt.bin")
@@ -36,13 +36,13 @@ class PrecompiledDebugProbesTest {
         }
     }
 
-    private fun assertJava6Compliance(classBytes: ByteArray) {
+    private fun assertJava8Compliance(classBytes: ByteArray) {
         DataInputStream(classBytes.inputStream()).use {
             val magic: Int = it.readInt()
             if (magic != -0x35014542) throw IllegalArgumentException("Not a valid class!")
             val minor: Int = it.readUnsignedShort()
             val major: Int = it.readUnsignedShort()
-            assertEquals(50, major)
+            assertEquals(52, major)
             assertEquals(0, minor)
         }
     }
