44dd7080e5f83ef06c437016d6532db9f0289e16
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2023-03-17 20:55:40 +0100

    Use cache redirector for download yarn and node

--- buildSrc/src/main/kotlin/CacheRedirector.kt
+++ buildSrc/src/main/kotlin/CacheRedirector.kt
@@ -26,10 +26,12 @@ private val mirroredUrls = listOf(
     "https://dl.google.com/dl/android/studio/ide-zips",
     "https://dl.google.com/go",
     "https://download.jetbrains.com",
+    "https://github.com/yarnpkg/yarn/releases/download",
     "https://jitpack.io",
     "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap",
     "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev",
     "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/eap",
+    "https://nodejs.org/dist",
     "https://oss.sonatype.org/content/repositories/releases",
     "https://oss.sonatype.org/content/repositories/snapshots",
     "https://oss.sonatype.org/content/repositories/staging",
@@ -50,7 +52,7 @@ private val mirroredUrls = listOf(
     "https://www.myget.org/F/intellij-go-snapshots/maven",
     "https://www.myget.org/F/rd-model-snapshots/maven",
     "https://www.myget.org/F/rd-snapshots/maven",
-    "https://www.python.org/ftp"
+    "https://www.python.org/ftp",
 )
 
 private val aliases = mapOf(
@@ -115,4 +117,13 @@ object CacheRedirector {
     fun Project.configure() {
         checkRedirect(repositories, displayName)
     }
+
+    @JvmStatic
+    fun maybeRedirect(url: String): String {
+        if (!cacheRedirectorEnabled) return url
+        return URI(url).maybeRedirect()?.toString() ?: url
+    }
+
+    @JvmStatic
+    val isEnabled get() = cacheRedirectorEnabled
 }
