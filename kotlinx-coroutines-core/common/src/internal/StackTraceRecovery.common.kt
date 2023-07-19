ef623b84910380272620cf6fe66af602550f3c47
Roman Efremov <roman.efremov@jetbrains.com>
2023-07-19 15:41:11 +0200

    Revert "Remove `@PublishedApi` from `unwrap` to comply with new compiler restriction (#3810)"
    This reverts commit 2bd0f2909a74cb2b2ba315c249b4f62166ca0268.

--- kotlinx-coroutines-core/common/src/internal/StackTraceRecovery.common.kt
+++ kotlinx-coroutines-core/common/src/internal/StackTraceRecovery.common.kt
@@ -40,6 +40,7 @@ internal expect suspend inline fun recoverAndThrow(exception: Throwable): Nothin
  * The opposite of [recoverStackTrace].
  * It is guaranteed that `unwrap(recoverStackTrace(e)) === e`
  */
+@PublishedApi // published for the multiplatform implementation of kotlinx-coroutines-test
 internal expect fun <E: Throwable> unwrap(exception: E): E
 
 internal expect class StackTraceElement
