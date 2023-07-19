466ba0808dc8bf484f1f268ad5a4ffca42d7ea2a
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2023-07-19 20:55:39 +0200

    Add more suppressions

--- kotlinx-coroutines-test/js/src/TestBuilders.kt
+++ kotlinx-coroutines-test/js/src/TestBuilders.kt
@@ -6,7 +6,7 @@ package kotlinx.coroutines.test
 import kotlinx.coroutines.*
 import kotlin.js.*
 
-@Suppress("ACTUAL_WITHOUT_EXPECT", "ACTUAL_TYPE_ALIAS_TO_CLASS_WITH_DECLARATION_SITE_VARIANCE")
+@Suppress("ACTUAL_WITHOUT_EXPECT", "ACTUAL_TYPE_ALIAS_TO_CLASS_WITH_DECLARATION_SITE_VARIANCE", "ACTUAL_TYPE_ALIAS_WITH_COMPLEX_SUBSTITUTION")
 public actual typealias TestResult = Promise<Unit>
 
 internal actual fun createTestResult(testProcedure: suspend CoroutineScope.() -> Unit): TestResult =
