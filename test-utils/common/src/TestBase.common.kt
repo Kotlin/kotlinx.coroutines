ee875a35e15c544212636f42aa55dcba0c9d38cc
Dmitry Khalanskiy <dmitry.khalanskiy@jetbrains.com>
2023-07-25 10:42:12 +0200

    K2: Fix Class 'TestBase' is not abstract and does not implement abstract member 'hasError'.

--- test-utils/common/src/TestBase.common.kt
+++ test-utils/common/src/TestBase.common.kt
@@ -225,6 +225,9 @@ public expect open class TestBase(): OrderedExecutionTestBase, ErrorCatching {
         unhandled: List<(Throwable) -> Boolean> = emptyList(),
         block: suspend CoroutineScope.() -> Unit
     ): TestResult
+
+    override fun hasError(): Boolean
+    override fun reportError(error: Throwable)
 }
 
 public suspend inline fun hang(onCancellation: () -> Unit) {
