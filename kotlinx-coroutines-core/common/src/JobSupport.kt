cb799093383ecfc676dc1cce6d9029c14b8e1c92
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2023-12-19 14:27:05 +0100

    Reduce the visibility of some API endpoints

--- kotlinx-coroutines-core/common/src/JobSupport.kt
+++ kotlinx-coroutines-core/common/src/JobSupport.kt
@@ -148,7 +148,6 @@ public open class JobSupport constructor(active: Boolean) : Job, ChildJob, Paren
             return
         }
         parent.start() // make sure the parent is started
-        @Suppress("DEPRECATION")
         val handle = parent.attachChild(this)
         parentHandle = handle
         // now check our state _after_ registering (see tryFinalizeSimpleState order of actions)
@@ -1175,7 +1174,7 @@ public open class JobSupport constructor(active: Boolean) : Job, ChildJob, Paren
             return parent.getCancellationException()
         }
 
-        protected override fun nameString(): String =
+        override fun nameString(): String =
             "AwaitContinuation"
     }
 
@@ -1372,7 +1371,7 @@ internal class NodeList : LockFreeLinkedListHead(), Incomplete {
         if (DEBUG) getString("Active") else super.toString()
 }
 
-internal class InactiveNodeList(
+private class InactiveNodeList(
     override val list: NodeList
 ) : Incomplete {
     override val isActive: Boolean get() = false
@@ -1408,12 +1407,6 @@ private class ResumeAwaitOnCompletion<T>(
     }
 }
 
-internal class DisposeOnCompletion(
-    private val handle: DisposableHandle
-) : JobNode() {
-    override fun invoke(cause: Throwable?) = handle.dispose()
-}
-
 // -------- invokeOnCancellation nodes
 
 /**
@@ -1432,7 +1425,7 @@ private class InvokeOnCancelling(
     }
 }
 
-internal class ChildHandleNode(
+private class ChildHandleNode(
     @JvmField val childJob: ChildJob
 ) : JobCancellingNode(), ChildHandle {
     override val parent: Job get() = job
