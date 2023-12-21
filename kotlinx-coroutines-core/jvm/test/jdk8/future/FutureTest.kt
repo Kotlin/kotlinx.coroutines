a40e2c565dad8f33bf3d2ee5c5ad474d1a0531a2
Margarita Bobova <margarita.bobova@jetbrains.com>
2023-12-21 14:08:37 +0100

    Temporary Ignore testStackOverflow because of KT-64075

--- kotlinx-coroutines-core/jvm/test/jdk8/future/FutureTest.kt
+++ kotlinx-coroutines-core/jvm/test/jdk8/future/FutureTest.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.testing.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.CancellationException
 import org.junit.*
+import org.junit.Ignore
 import org.junit.Test
 import java.lang.IllegalArgumentException
 import java.util.concurrent.*
@@ -571,6 +572,7 @@ class FutureTest : TestBase() {
     }
 
     @Test
+    @Ignore // Temporary ignored because of KT-64075
     fun testStackOverflow() = runTest {
         val future = CompletableFuture<Int>()
         val completed = AtomicLong()
