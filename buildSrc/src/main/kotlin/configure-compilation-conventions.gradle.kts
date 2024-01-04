bbb5c3d8ba03b9772975c22329ddf4e8863c9253
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-01-04 10:00:52 +0100

    Cleanup configure-compilation-conventions.gradle.kts
    This was an attempt to fix IDE complaining about not having the
    right opt-in in kotlinx-coroutines-test/common/test/Helpers.kt, but
    this failed.

--- buildSrc/src/main/kotlin/configure-compilation-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/configure-compilation-conventions.gradle.kts
@@ -2,10 +2,7 @@
  * Copyright 2016-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import org.jetbrains.kotlin.gradle.dsl.*
-import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
 import org.jetbrains.kotlin.gradle.tasks.*
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 configure(subprojects) {
     val project = this
@@ -27,22 +24,17 @@ configure(subprojects) {
                 allWarningsAsErrors = true
                 freeCompilerArgs.add("-Xexplicit-api=strict")
             }
-
-            /*
-             * Coroutines do not interop with Java and these flags provide a significant
-             * (i.e. close to double-digit) reduction in both bytecode and optimized dex size
-             */
-            val bytecodeSizeReductionOptions =
-                if (this@configureEach is KotlinJvmCompile) listOf(
+            /* Coroutines do not interop with Java and these flags provide a significant
+             * (i.e. close to double-digit) reduction in both bytecode and optimized dex size */
+            if (this@configureEach is KotlinJvmCompile) {
+                freeCompilerArgs.addAll(
                     "-Xno-param-assertions",
                     "-Xno-call-assertions",
                     "-Xno-receiver-assertions"
-                ) else emptyList()
-
-            val newOptions = listOf(
-                "-progressive", "-Xexpect-actual-classes"
-            ) + bytecodeSizeReductionOptions + optInAnnotations.map { "-opt-in=$it" }
-            freeCompilerArgs.addAll(newOptions)
+                )
+            }
+            freeCompilerArgs.addAll("-progressive", "-Xexpect-actual-classes")
+            optIn.addAll(optInAnnotations)
         }
 
     }
