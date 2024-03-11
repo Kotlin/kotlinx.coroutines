29e821302f2b18b75252489997cc22d4a3fbdc09
mvicsokolova <82594708+mvicsokolova@users.noreply.github.com>
2024-03-11 08:47:28 +0100

    Ensure that inline functions only access atomics from the same class (#4041)
    Calling a function containing atomic operations from a different file results in the failure of Native incremental compilation.
    
    Made BufferedChannel#sendImpl function private.
    
    This is a WA for KT-65554

--- kotlinx-coroutines-core/common/src/channels/BufferedChannel.kt
+++ kotlinx-coroutines-core/common/src/channels/BufferedChannel.kt
@@ -241,7 +241,7 @@ internal open class BufferedChannel<E>(
     /**
      * Abstract send implementation.
      */
-    protected inline fun <R> sendImpl(
+    private inline fun <R> sendImpl(
         /* The element to be sent. */
         element: E,
         /* The waiter to be stored in case of suspension,
@@ -349,6 +349,29 @@ internal open class BufferedChannel<E>(
             }
         }
     }
+
+    // Note: this function is temporarily moved from ConflatedBufferedChannel to BufferedChannel class, because of this issue: KT-65554. 
+    // For now, an inline function, which invokes atomic operations, may only be called within a parent class.
+    protected fun trySendDropOldest(element: E): ChannelResult<Unit> =
+        sendImpl( // <-- this is an inline function
+            element = element,
+            // Put the element into the logical buffer even
+            // if this channel is already full, the `onSuspend`
+            // callback below extract the first (oldest) element.
+            waiter = BUFFERED,
+            // Finish successfully when a rendezvous has happened
+            // or the element has been buffered.
+            onRendezvousOrBuffered = { return success(Unit) },
+            // In case the algorithm decided to suspend, the element
+            // was added to the buffer. However, as the buffer is now
+            // overflowed, the first (oldest) element has to be extracted.
+            onSuspend = { segm, i ->
+                dropFirstElementUntilTheSpecifiedCellIsInTheBuffer(segm.id * SEGMENT_SIZE + i)
+                return success(Unit)
+            },
+            // If the channel is closed, return the corresponding result.
+            onClosed = { return closed(sendException) }
+        )
 
     private inline fun sendImplOnNoWaiter(
         /* The working cell is specified by
