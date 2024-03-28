ce9036d6d0f6d6a8791c16eea8e6ed09f07c4e83
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-03-28 11:29:22 +0100

    Add doc for BlockingDispatchAware

--- kotlinx-coroutines-core/common/src/internal/BlockingDispatchAware.kt
+++ kotlinx-coroutines-core/common/src/internal/BlockingDispatchAware.kt
@@ -1,7 +1,22 @@
 package kotlinx.coroutines.internal
 
-
+/**
+ * [Runnables][kotlinx.coroutines.Runnable] that are dispatched on [CoroutineDispatcher][kotlinx.coroutines.CoroutineDispatcher]
+ * may optionally implement this interface to be notified of dispatch emulation in blocking mode.
+ *
+ * This may be needed for controlling dispatcher to release/acquire a permit of the worker that currently
+ * executes the dispatched Runnable.
+ * @see LimitedDispatcher.Worker
+ * @see kotlinx.coroutines.scheduling.withUnlimitedIOScheduler
+ */
 internal interface BlockingDispatchAware {
+    /**
+     * Must not throw
+     */
     fun beforeDispatchElsewhere()
+
+    /**
+     * Must not throw
+     */
     fun afterDispatchBack()
 }
