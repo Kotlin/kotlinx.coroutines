20bf726d5757fadc9bbb1c763a785c68c9e8f762
Dmitry Khalanskiy <52952525+dkhalanskyjb@users.noreply.github.com>
2024-04-03 13:34:22 +0200

    Mark the coroutine started with `UNDISPATCHED` as running (#4077)
    Fixes #4058
    
    Additional small fix: the coroutine context injected by `probeCoroutineCreated` is used for `CoroutineStart.UNDISPATCHED` even before the first suspension.

--- kotlinx-coroutines-core/common/src/internal/ProbesSupport.common.kt
+++ kotlinx-coroutines-core/common/src/internal/ProbesSupport.common.kt
@@ -3,3 +3,5 @@ package kotlinx.coroutines.internal
 import kotlin.coroutines.*
 
 internal expect inline fun <T> probeCoroutineCreated(completion: Continuation<T>): Continuation<T>
+
+internal expect inline fun <T> probeCoroutineResumed(completion: Continuation<T>): Unit
--- kotlinx-coroutines-core/common/test/EmptyContext.kt
+++ kotlinx-coroutines-core/common/test/EmptyContext.kt
@@ -1,8 +1,30 @@
 package kotlinx.coroutines
 
-import kotlinx.coroutines.intrinsics.*
+import kotlinx.coroutines.internal.probeCoroutineCreated
+import kotlinx.coroutines.internal.probeCoroutineResumed
 import kotlin.coroutines.*
+import kotlin.coroutines.intrinsics.*
 
 suspend fun <T> withEmptyContext(block: suspend () -> T): T = suspendCoroutine { cont ->
     block.startCoroutineUnintercepted(Continuation(EmptyCoroutineContext) { cont.resumeWith(it) })
+}
+
+/**
+ * Use this function to restart a coroutine directly from inside of [suspendCoroutine],
+ * when the code is already in the context of this coroutine.
+ * It does not use [ContinuationInterceptor] and does not update the context of the current thread.
+ */
+fun <T> (suspend () -> T).startCoroutineUnintercepted(completion: Continuation<T>) {
+    val actualCompletion = probeCoroutineCreated(completion)
+    val value = try {
+        probeCoroutineResumed(actualCompletion)
+        startCoroutineUninterceptedOrReturn(actualCompletion)
+    } catch (e: Throwable) {
+        actualCompletion.resumeWithException(e)
+        return
+    }
+    if (value !== COROUTINE_SUSPENDED) {
+        @Suppress("UNCHECKED_CAST")
+        actualCompletion.resume(value as T)
+    }
 }
