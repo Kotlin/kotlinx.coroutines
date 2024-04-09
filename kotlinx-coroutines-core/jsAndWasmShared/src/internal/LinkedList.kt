d4cb5fa6f0e751ce713be9144ff70aad22696eb2
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-04-09 13:01:51 +0200

    Minimize the API surface of `LockFreeLinkedList`
    With this change, `JobSupport` uses only a small and well-defined
    portion of the functionality `LockFreeLinkedList` provides, which
    makes it easier to replace the list implementation.

--- kotlinx-coroutines-core/jsAndWasmShared/src/internal/LinkedList.kt
+++ kotlinx-coroutines-core/jsAndWasmShared/src/internal/LinkedList.kt
@@ -2,6 +2,8 @@
 
 package kotlinx.coroutines.internal
 
+import kotlinx.coroutines.*
+
 private typealias Node = LockFreeLinkedListNode
 
 /** @suppress **This is unstable API and it is subject to change.** */
@@ -10,11 +12,7 @@ public actual open class LockFreeLinkedListNode {
     @PublishedApi internal var _prev = this
     @PublishedApi internal var _removed: Boolean = false
 
-    public actual inline val nextNode get() = _next
-    inline actual val prevNode get() = _prev
-    inline actual val isRemoved get() = _removed
-
-    public actual fun addLast(node: Node, permissionsBitmask: Int): Boolean = when (val prev = this._prev) {
+    fun addLast(node: Node, permissionsBitmask: Int): Boolean = when (val prev = this._prev) {
         is ListClosed ->
             prev.forbiddenElementsBitmask and permissionsBitmask == 0 && prev.addLast(node, permissionsBitmask)
         else -> {
@@ -26,30 +24,26 @@ public actual open class LockFreeLinkedListNode {
         }
     }
 
-    public actual fun close(forbiddenElementsBit: Int) {
-        addLast(ListClosed(forbiddenElementsBit), forbiddenElementsBit)
-    }
-
     /*
      * Remove that is invoked as a virtual function with a
      * potentially augmented behaviour.
      * I.g. `LockFreeLinkedListHead` throws, while `SendElementWithUndeliveredHandler`
      * invokes handler on remove
      */
-    public actual open fun remove(): Boolean {
-        if (_removed) return false
+    public actual open fun remove() {
+        if (_removed) return
         val prev = this._prev
         val next = this._next
         prev._next = next
         next._prev = prev
         _removed = true
-        return true
     }
 
-    public actual fun addOneIfEmpty(node: Node): Boolean {
-        if (_next !== this) return false
-        addLast(node, Int.MIN_VALUE)
-        return true
+    public actual fun attachToList(node: LockFreeLinkedListHead): Node {
+        if (_next !== this) return _next
+        val success = addLast(node, Int.MIN_VALUE)
+        assert { success }
+        return node
     }
 }
 
@@ -58,16 +52,24 @@ public actual open class LockFreeLinkedListHead : Node() {
     /**
      * Iterates over all elements in this list of a specified type.
      */
-    public actual inline fun forEach(block: (Node) -> Unit) {
-        var cur: Node = _next
-        while (cur != this) {
-            block(cur)
+    public actual inline fun forEach(startAfter: LockFreeLinkedListNode?, block: (Node) -> Unit) {
+        var cur: Node = startAfter ?: this
+        while (cur._removed) cur = cur._prev // rollback to prev non-removed (or list head)
+        cur = cur._next
+        while (cur !== this) {
+            if (!cur._removed) {
+                block(cur)
+            }
             cur = cur._next
         }
     }
 
+    public actual fun close(forbiddenElementsBit: Int) {
+        addLast(ListClosed(forbiddenElementsBit), forbiddenElementsBit)
+    }
+
     // just a defensive programming -- makes sure that list head sentinel is never removed
-    public actual final override fun remove(): Nothing = throw UnsupportedOperationException()
+    public final override fun remove(): Nothing = throw UnsupportedOperationException()
 }
 
 private class ListClosed(val forbiddenElementsBitmask: Int): LockFreeLinkedListNode()
