4556e76d09074d70941f1348e5c4302b36a32091
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2024-04-23 12:37:47 +0200

    Add a workaround for incremental compilation bug (#4114)
    See KT-66317

--- kotlinx-coroutines-core/jvm/src/internal/Concurrent.kt
+++ kotlinx-coroutines-core/jvm/src/internal/Concurrent.kt
@@ -13,6 +13,12 @@ internal actual inline fun <T> ReentrantLock.withLock(action: () -> T) = this.wi
 @Suppress("ACTUAL_WITHOUT_EXPECT") // Visibility
 internal actual typealias WorkaroundAtomicReference<V> = java.util.concurrent.atomic.AtomicReference<V>
 
+// BenignDataRace is OptionalExpectation and doesn't have to be here
+// but then IC breaks. See KT-66317
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.FIELD)
+internal actual annotation class BenignDataRace()
+
 @Suppress("NOTHING_TO_INLINE") // So that R8 can completely remove ConcurrentKt class
 internal actual inline fun <E> identitySet(expectedSize: Int): MutableSet<E> =
     Collections.newSetFromMap(IdentityHashMap(expectedSize))
