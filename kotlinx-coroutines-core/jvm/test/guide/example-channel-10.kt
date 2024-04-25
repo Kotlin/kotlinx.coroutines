c1ba5af8c5d10a3d2f923f89b98f6ab1b394e24d
Dmitry Khalanskiy <52952525+dkhalanskyjb@users.noreply.github.com>
2024-04-25 10:49:04 +0200

    Fix the ticker channel example giving wrong results on the website (#4109)
    https://play.kotlinlang.org/ of the example that's being changed
    here is currently unreliable: the last line is occasionally `null`.
    
    By increasing all time intervals twofold, we reduce the impact of
    the CPU scheduling in the constrained environment. With this
    change, the results are consistent across dozens of runs:
    <https://pl.kotl.in/uCa-60j43>
    
    Originally reported by `@PetrakovichVictoria`

--- kotlinx-coroutines-core/jvm/test/guide/example-channel-10.kt
+++ kotlinx-coroutines-core/jvm/test/guide/example-channel-10.kt
@@ -5,25 +5,25 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.*
 
 fun main() = runBlocking<Unit> {
-    val tickerChannel = ticker(delayMillis = 100, initialDelayMillis = 0) // create ticker channel
+    val tickerChannel = ticker(delayMillis = 200, initialDelayMillis = 0) // create a ticker channel
     var nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
     println("Initial element is available immediately: $nextElement") // no initial delay
 
-    nextElement = withTimeoutOrNull(50) { tickerChannel.receive() } // all subsequent elements have 100ms delay
-    println("Next element is not ready in 50 ms: $nextElement")
+    nextElement = withTimeoutOrNull(100) { tickerChannel.receive() } // all subsequent elements have 200ms delay
+    println("Next element is not ready in 100 ms: $nextElement")
 
-    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
-    println("Next element is ready in 100 ms: $nextElement")
+    nextElement = withTimeoutOrNull(120) { tickerChannel.receive() }
+    println("Next element is ready in 200 ms: $nextElement")
 
     // Emulate large consumption delays
-    println("Consumer pauses for 150ms")
-    delay(150)
+    println("Consumer pauses for 300ms")
+    delay(300)
     // Next element is available immediately
     nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
     println("Next element is available immediately after large consumer delay: $nextElement")
     // Note that the pause between `receive` calls is taken into account and next element arrives faster
-    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() } 
-    println("Next element is ready in 50ms after consumer pause in 150ms: $nextElement")
+    nextElement = withTimeoutOrNull(120) { tickerChannel.receive() }
+    println("Next element is ready in 100ms after consumer pause in 300ms: $nextElement")
 
     tickerChannel.cancel() // indicate that no more elements are needed
 }
