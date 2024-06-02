8cafba256e49006469844f1622bea7318fc57cf8
Awkin Globsterg <globsterg@hotmail.com>
2024-06-02 16:22:06 +0200

    Reword the BufferOverflow KDoc for consistency in the entry list
    Before, the description of `SUSPEND` was phrased in terms of what
    will happen, while the rest of the entries were described in an
    imperative form, that is, as commands as to what should happen.
    Now, all entries are clarified using a descriptive form.

--- kotlinx-coroutines-core/common/src/channels/BufferOverflow.kt
+++ kotlinx-coroutines-core/common/src/channels/BufferOverflow.kt
@@ -6,9 +6,10 @@ package kotlinx.coroutines.channels
  *
  * - [SUSPEND] &mdash; the upstream that is [sending][SendChannel.send] or
  *   is [emitting][kotlinx.coroutines.flow.FlowCollector.emit] a value is **suspended** while the buffer is full.
- * - [DROP_OLDEST] &mdash; drop **the oldest** value in the buffer on overflow, add the new value to the buffer, do not suspend.
- * - [DROP_LATEST] &mdash; drop **the latest** value that is being added to the buffer right now on buffer overflow
- *   (so that buffer contents stay the same), do not suspend.
+ * - [DROP_OLDEST] &mdash; **the oldest** value in the buffer is dropped on overflow, and the new value is added,
+ *   all without suspending.
+ * - [DROP_LATEST] &mdash; the buffer remains unchanged on overflow, and the value that we were going to add
+ *   gets discarded, all without suspending.
  */
 public enum class BufferOverflow {
     /**
