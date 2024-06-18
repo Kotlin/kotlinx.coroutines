3d92dd6da1be803c4b19bfd24c2810ee7e3650ec
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-06-18 13:06:38 +0300

    Turned off optimization when compiling tests for K/N
    Optimization was turned on due to performance problems. However, with new K/N memory manager
    these performance problems seem to be fixed. On the other hand, compiling in optimized mode
    significantly increases the compiler's memory consumption, so it's better to turn it off.

--- kotlinx-coroutines-test/build.gradle.kts
+++ kotlinx-coroutines-test/build.gradle.kts
@@ -2,12 +2,6 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.*
 import org.jetbrains.kotlin.gradle.targets.js.dsl.*
 
 kotlin {
-    targets.withType(KotlinNativeTargetWithTests::class.java).configureEach {
-        binaries.getTest("DEBUG").apply {
-            optimized = true
-        }
-    }
-
     sourceSets {
         jvmTest {
             dependencies {
