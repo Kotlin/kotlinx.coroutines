0f4b5469e4924c9ef3ff5f0d5a12098fc5e69f1f
igoriakovlev <54274820+igoriakovlev@users.noreply.github.com>
2024-07-09 10:20:55 +0200

    Remove w3c dependencies from wasmJs target (#4097)
    See https://youtrack.jetbrains.com/issue/KT-54299/Extract-org.w3c-declarations-to-separate-library-from-K-Wasm-Stdlib

--- kotlinx-coroutines-core/wasmJs/src/CoroutineContext.kt
+++ kotlinx-coroutines-core/wasmJs/src/CoroutineContext.kt
@@ -1,7 +1,5 @@
 package kotlinx.coroutines
 
-import org.w3c.dom.*
-
 internal external interface JsProcess : JsAny {
     fun nextTick(handler: () -> Unit)
 }
@@ -9,7 +7,7 @@ internal external interface JsProcess : JsAny {
 internal fun tryGetProcess(): JsProcess? =
     js("(typeof(process) !== 'undefined' && typeof(process.nextTick) === 'function') ? process : null")
 
-internal fun tryGetWindow(): Window? =
+internal fun tryGetWindow(): W3CWindow? =
     js("(typeof(window) !== 'undefined' && window != null && typeof(window.addEventListener) === 'function') ? window : null")
 
 internal actual fun createDefaultDispatcher(): CoroutineDispatcher =
