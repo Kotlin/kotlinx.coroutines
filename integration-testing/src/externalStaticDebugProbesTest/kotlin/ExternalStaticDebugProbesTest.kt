c2468a95828f42fad96e7ed9c469896606c91193
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-10-10 08:13:00 +0200

    Support 'ExternalStaticDebugProbes' (in collaboration with the kotlin-stdlib)

---
+++ integration-testing/src/externalStaticDebugProbesTest/kotlin/ExternalStaticDebugProbesTest.kt
@@ -0,0 +1,21 @@
+import org.junit.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.external.ExternalStaticDebugProbes
+import org.junit.Test
+import java.io.*
+
+class ExternalStaticDebugProbesTest {
+
+    @Test
+    fun testDumpCoroutines() {
+        println("Hello")
+        runBlocking {
+            val baos = ByteArrayOutputStream()
+            ExternalStaticDebugProbes.dumpCoroutines(PrintStream(baos))
+            // if the agent works, then dumps should contain something,
+            // at least the fact that this test is running.
+            val dump = baos.toString()
+            Assert.assertTrue(dump, dump.contains("testDumpCoroutines"))
+        }
+    }
+}
