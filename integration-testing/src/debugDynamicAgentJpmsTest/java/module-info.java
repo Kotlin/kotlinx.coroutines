ae0ba4009e6170c8fe0f13529e78d40fadb2a721
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-10-16 07:03:11 +0200

    fixup! kotlinx-coroutines-debug: Fix split-package with the -core module

--- integration-testing/src/debugDynamicAgentJpmsTest/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module debug.dynamic.agent.jpms.test {
-    requires kotlin.stdlib;
-    requires kotlinx.coroutines.core;
-    requires kotlinx.coroutines.debug;
-    requires junit;
-    requires kotlin.test;
-}
