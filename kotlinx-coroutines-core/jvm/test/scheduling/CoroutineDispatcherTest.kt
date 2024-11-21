27067fcaa645e3aeb92360709bafd7ce5156ada0
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-11-21 13:53:10 +0100

    Cleanup

--- kotlinx-coroutines-core/jvm/test/scheduling/CoroutineDispatcherTest.kt
+++ kotlinx-coroutines-core/jvm/test/scheduling/CoroutineDispatcherTest.kt
@@ -16,22 +16,17 @@ class CoroutineDispatcherTest : SchedulerTestBase() {
     @Test
     fun testSingleThread() = runBlocking {
         corePoolSize = 1
-        println("1. Thread is ${Thread.currentThread()}")
         expect(1)
         withContext(dispatcher) {
             require(Thread.currentThread() is CoroutineScheduler.Worker)
-            println("2. Thread is ${Thread.currentThread()}")
             expect(2)
             val job = async {
-                println("3. Thread is ${Thread.currentThread()}")
                 expect(3)
                 delay(10)
-                println("4. Thread is ${Thread.currentThread()}")
                 expect(4)
             }
 
             job.await()
-            println("5. Thread is ${Thread.currentThread()}")
             expect(5)
         }
 
