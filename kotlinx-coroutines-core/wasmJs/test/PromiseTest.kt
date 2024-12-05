3ae4abdb9a221317cdb930b309c9503c881c5b0b
Alex Shepeliev <a.shepeliev@gmail.com>
2024-12-05 18:30:43 +0200

    Add tests

--- kotlinx-coroutines-core/wasmJs/test/PromiseTest.kt
+++ kotlinx-coroutines-core/wasmJs/test/PromiseTest.kt
@@ -86,13 +86,25 @@ class PromiseTest : TestBase() {
     }
 
     @Test
-    fun testAwaitPromiseRejectedFromJs() = GlobalScope.promise {
+    fun testAwaitPromiseRejectedWithNonKotlinException() = GlobalScope.promise {
         lateinit var r: (JsAny) -> Unit
         val toAwait = Promise<JsAny?> { _, reject -> r = reject }
         val throwable = async(start = CoroutineStart.UNDISPATCHED) {
             assertFails { toAwait.await<JsAny?>() }
         }
         r("Rejected".toJsString())
-        assertContains(throwable.await().message ?: "", "Rejected")
+        assertIs<JsException>(throwable.await())
+    }
+
+    @Test
+    fun testAwaitPromiseRejectedWithKotlinException() = GlobalScope.promise {
+        lateinit var r: (JsAny) -> Unit
+        val toAwait = Promise<JsAny?> { _, reject -> r = reject }
+        val throwable = async(start = CoroutineStart.UNDISPATCHED) {
+            assertFails { toAwait.await<JsAny?>() }
+        }
+        r(RuntimeException("Rejected").toJsReference())
+        assertIs<RuntimeException>(throwable.await())
+        assertEquals("Rejected", throwable.await().message)
     }
 }
