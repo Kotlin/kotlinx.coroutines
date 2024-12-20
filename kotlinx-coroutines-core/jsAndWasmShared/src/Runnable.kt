2a9b88c0ab6f38d19eff43d26482c5d1bb419380
Dmitry Khalanskiy <52952525+dkhalanskyjb@users.noreply.github.com>
2024-12-20 11:50:54 +0100

    Fix binary incompatibility introduced in #4261 (#4310)
    Fixes #4309

--- kotlinx-coroutines-core/jsAndWasmShared/src/Runnable.kt
+++ kotlinx-coroutines-core/jsAndWasmShared/src/Runnable.kt
@@ -11,3 +11,14 @@ public actual fun interface Runnable {
      */
     public actual fun run()
 }
+
+@Deprecated(
+    "Preserved for binary compatibility, see https://github.com/Kotlin/kotlinx.coroutines/issues/4309",
+    level = DeprecationLevel.HIDDEN
+)
+public inline fun Runnable(crossinline block: () -> Unit): Runnable =
+    object : Runnable {
+        override fun run() {
+            block()
+        }
+    }
