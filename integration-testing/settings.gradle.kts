3ed623a5706ac6f4a3bc191150e3ff5caa86eba3
Dmitry Tsyvtsyn <40917658+DmitryTsyvtsyn@users.noreply.github.com>
2025-02-18 17:26:47 +0700

    Migrate integration-testing module from Groovy to Kts (#4341)
    Fixes #4285

--- integration-testing/settings.gradle.kts
+++ integration-testing/settings.gradle.kts
@@ -1,14 +1,14 @@
 pluginManagement {
     repositories {
         mavenCentral()
-        maven { url "https://plugins.gradle.org/m2/" }
-        maven { url "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev" }
+        maven("https://plugins.gradle.org/m2/")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         mavenLocal()
     }
 }
 
-include 'smokeTest'
-include 'java8Test'
+include("smokeTest")
+include("java8Test")
 include(":jpmsTest")
 
 rootProject.name = "kotlinx-coroutines-integration-testing"
