0709d895c4c25ea024ea3226c738dec950aca425
Tatiana Bogdanova <Tatiana.Bogdanova@jetbrains.com>
2025-02-26 18:24:13 +0100

    Add new kotlin-dev repo url

--- integration-testing/settings.gradle.kts
+++ integration-testing/settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         mavenCentral()
         maven("https://plugins.gradle.org/m2/")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        maven("https://redirector.kotlinlang.org/maven/dev")
         mavenLocal()
     }
 }
