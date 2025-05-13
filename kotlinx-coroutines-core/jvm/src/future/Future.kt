9f97a458b3c6f36a2a119e23219d7973792e8006
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2025-05-13 15:25:20 +0200

    Introduce deprecated coroutine builder overloads accepting a `Job`
    This is a small step towards deprecating the practice of breaking
    structured concurrency by passing a `Job` to a coroutine builder.
    
    See #3670

--- kotlinx-coroutines-core/jvm/src/future/Future.kt
+++ kotlinx-coroutines-core/jvm/src/future/Future.kt
@@ -41,6 +41,25 @@ public fun <T> CoroutineScope.future(
     return future
 }
 
+/**
+ * Deprecated version of [future] that accepts a [Job].
+ *
+ * See the documentation for the non-deprecated [future] function to learn about the functionality of this function.
+ *
+ * See the documentation for the deprecated [async] overload accepting a [Job] for an explanation of the reason
+ * this pattern is deprecated and the list of possible alternatives.
+ */
+@Deprecated(
+    "Passing a Job to coroutine builders breaks structured concurrency, leading to hard-to-diagnose errors. " +
+        "This pattern should be avoided. " +
+        "This overload will be deprecated with an error in the future.",
+    level = DeprecationLevel.WARNING)
+public fun <T> CoroutineScope.future(
+    context: Job,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> T
+): CompletableFuture<T> = future(context as CoroutineContext, start, block)
+
 private class CompletableFutureCoroutine<T>(
     context: CoroutineContext,
     private val future: CompletableFuture<T>
