4e7cabd0d7870a495cde280ef4dcced91165561d
Natasha Murashkina <murfel@users.noreply.github.com>
2025-12-23 14:02:44 +0000

    relacing with stdlib atomics helps

--- test-utils/concurrent/src/ConcurrentCyclicBarrier.kt
+++ test-utils/concurrent/src/ConcurrentCyclicBarrier.kt
@@ -1,18 +1,23 @@
-import kotlinx.atomicfu.atomic
+
 import kotlinx.atomicfu.locks.ExperimentalThreadBlockingApi
 import kotlinx.atomicfu.locks.ParkingHandle
 import kotlinx.atomicfu.locks.ParkingSupport
+import kotlin.concurrent.atomics.AtomicBoolean
+import kotlin.concurrent.atomics.AtomicReference
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
 import kotlin.test.DefaultAsserter.fail
 import kotlin.time.Duration
 
-private class HandleWrapper @OptIn(ExperimentalThreadBlockingApi::class) constructor(val handle: ParkingHandle) {
-    val woken = atomic(false)
+@OptIn(ExperimentalThreadBlockingApi::class)
+private class HandleWrapper(val handle: ParkingHandle) {
+    @OptIn(ExperimentalAtomicApi::class)
+    val woken = AtomicBoolean(false)
 }
 
 public class ConcurrentCyclicBarrier(private val parties: Int) {
     private val queue = MSQueueCyclicBarrier<HandleWrapper>()
 
-    @OptIn(ExperimentalThreadBlockingApi::class)
+    @OptIn(ExperimentalThreadBlockingApi::class, ExperimentalAtomicApi::class)
     fun await() {
         val wrapper = HandleWrapper(ParkingSupport.currentThreadHandle())
         val n = queue.enqueue(wrapper)
@@ -20,41 +25,41 @@ public class ConcurrentCyclicBarrier(private val parties: Int) {
             var wokenUp = 0
             while (wokenUp < parties - 1) {
                 val deq = queue.dequeue()
-                if (deq == null) fail("Not enough parties enqueued")
-                if (deq.first % parties == 0L) continue
+                //if (deq == null) fail("Not enough parties enqueued")
+                if (deq!!.first % parties == 0L) continue
                 if (deq.second.woken.compareAndSet(false, true)) {
                     ParkingSupport.unpark(deq.second.handle)
                     wokenUp++
                 }
             }
         } else {
-            while (!wrapper.woken.value) {
+            while (!wrapper.woken.load()) {
                 ParkingSupport.park(Duration.INFINITE)
             }
         }
     }
 }
 
-
+@OptIn(ExperimentalAtomicApi::class)
 private class MSQueueCyclicBarrier<E> {
-    private val head = atomic(Node<E>(null, 0))
-    private val tail = atomic(head.value)
+    private val head = AtomicReference(Node<E>(null, 0))
+    private val tail = AtomicReference(head.load())
 
     fun enqueue(element: E): Long {
         while (true) {
-            val curTail = tail.value
+            val curTail = tail.load()
             val node = Node(element, curTail.id + 1)
             if (curTail.next.compareAndSet(null, node)) {
                 tail.compareAndSet(curTail, node)
                 return node.id
-            } else tail.compareAndSet(curTail, curTail.next.value!!)
+            } else tail.compareAndSet(curTail, curTail.next.load()!!)
         }
     }
 
     fun dequeue(): Pair<Long, E>? {
         while (true) {
-            val currentHead = head.value
-            val currentHeadNext = currentHead.next.value ?: return null
+            val currentHead = head.load()
+            val currentHeadNext = currentHead.next.load() ?: return null
             if (head.compareAndSet(currentHead, currentHeadNext)) {
                 val element = currentHeadNext.element
                 currentHeadNext.element = null
@@ -65,6 +70,6 @@ private class MSQueueCyclicBarrier<E> {
     }
 
     private class Node<E>(var element: E?, val id: Long) {
-        val next = atomic<Node<E>?>(null)
+        val next = AtomicReference<Node<E>?>(null)
     }
 }
