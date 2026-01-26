ff25be1dfc7bd0a79d9bd6abcd17c18e064c866b
Natasha Murashkina <murfel@users.noreply.github.com>
2026-01-26 13:13:02 +0000

    fix FieldWalker usage - add assertTrueJvm

--- test-utils/jvm/src/ConcurrentThreadUtils.kt
+++ test-utils/jvm/src/ConcurrentThreadUtils.kt
@@ -2,6 +2,8 @@ package kotlinx.coroutines.testing
 
 import kotlin.concurrent.*
 
+actual fun assertTrueJvm(value: Boolean) = kotlin.test.assertTrue(value)
+
 actual fun runThread(
     name: String?, block: () -> Unit
 ): ConcurrentThread {
