e1a26c0f55383ebc10e66f66e3f93f354052f724
Natasha Murashkina <murfel@users.noreply.github.com>
2026-01-27 21:36:09 +0000

    move PoolSizes to test/

--- kotlinx-coroutines-core/concurrent/src/scheduling/PoolSizes.concurrent.kt
+++
@@ -1,5 +0,0 @@
-package kotlinx.coroutines.scheduling
-
-// Used for testing only, but is in src/... due to JVM implementation relying on internal constants from src.
-internal expect val CONCURRENT_CORE_POOL_SIZE: Int
-internal expect val CONCURRENT_MAX_POOL_SIZE: Int
