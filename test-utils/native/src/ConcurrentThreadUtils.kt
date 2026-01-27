14e3c0b3d1483a41df3075b6e36c532b5f743770
Natasha Murashkina <murfel@users.noreply.github.com>
2026-01-27 11:44:57 +0000

    fix ConcurrentThread repeat start and join

--- test-utils/native/src/ConcurrentThreadUtils.kt
+++ test-utils/native/src/ConcurrentThreadUtils.kt
@@ -1,30 +1,40 @@
+@file:OptIn(ExperimentalAtomicApi::class)
+
 package kotlinx.coroutines.testing
 
+import kotlinx.coroutines.*
+import kotlin.concurrent.atomics.*
 import kotlin.native.concurrent.*
 
 actual fun assertTrueJvm(value: Boolean) = Unit
 
 actual fun runThread(
     name: String?, block: () -> Unit
-): ConcurrentThread {
-    return ConcurrentThread(name, block).apply { start() }
-}
+): ConcurrentThread = ConcurrentThread(block, name).apply { start() }
 
 actual class ConcurrentThread actual constructor(
-    private val name: String?, private val block: () -> Unit
+    private val block: Runnable, private val name: String?
 ) {
+    private val starting = AtomicBoolean(false)
+    private val started = AtomicBoolean(false)
+    private val joined = AtomicBoolean(false)
     private var worker: Worker? = null
     private var future: Future<Unit>? = null
 
     actual fun start() {
-        worker = Worker.start(name = name ?: "TestWorker")
-        future = worker!!.execute(TransferMode.SAFE, { block }) {
-            it()
+        if (!starting.compareAndSet(false, true)) {
+            throw IllegalStateException("Cannot call start() on a ConcurrentThread which has already started")
         }
+        worker = Worker.start(name = name)
+        future = worker!!.execute(TransferMode.SAFE, { block }) { it.run() }
+        started.store(true)
     }
 
     actual fun join() {
-        future?.consume { }
-        worker?.requestTermination()
+        if (started.load() && !joined.load()) {
+            future!!.consume { }
+            worker!!.requestTermination().result
+            joined.store(true)
+        }
     }
 }
