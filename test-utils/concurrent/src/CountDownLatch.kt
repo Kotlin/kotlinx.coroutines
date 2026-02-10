8ceb8a8f4902bb0427b3b9da2de608c23da7205f
Natasha Murashkina <murfel@users.noreply.github.com>
2026-02-10 09:57:56 +0100

    revert CountDownLatch to atomicfu

--- test-utils/concurrent/src/CountDownLatch.kt
+++ test-utils/concurrent/src/CountDownLatch.kt
@@ -9,60 +9,53 @@ import kotlin.time.*
 
 class CountDownLatch(count: Int) {
     private val c = atomic(count)
-    private val waiters = MpscStack()
+    private val waiters = MSQueueLatch<ParkingHandle>()
 
     fun await() {
-        val handle = ParkingSupport.currentThreadHandle()
-        if (waiters.push(handle)) {
-            // Each pushed thread must be parked at least once.
-            // Unpark before park makes the next park return immediately.
-            // Unpark will be definitely called on each pushed thread in `drain`.
-            // So we must counteract the side effects of this unpark.
-            do {
-                ParkingSupport.park(Duration.INFINITE)
-            } while (c.value > 0)
-        }
+        val thread = ParkingSupport.currentThreadHandle()
+        waiters.enqueue(thread)
+        while (c.value > 0) ParkingSupport.park(Duration.INFINITE)
     }
 
     fun countDown() {
-        val count = c.decrementAndGet()
-        if (count > 0) return
-        if (count < 0) error("Count down to negative value: $count is prohibited.")
-        // count == 0, c <= 0
-        waiters.drain {
-            ParkingSupport.unpark(it)
+        val myIndex = c.decrementAndGet()
+        if (myIndex != 0) return
+        while (true) {
+            val thread = waiters.dequeue()
+            if (thread == null) return
+            ParkingSupport.unpark(thread)
         }
     }
 }
 
-private class MpscStack {
-    // Invariant: node only stores non-null values. Node(null, _) is a sentinel.
-    private class Node(val data: ParkingHandle?, var next: Node?)
+private class MSQueueLatch<E> {
+    private val head = atomic(Node<E>(null))
+    private val tail = atomic(head.value)
 
-    companion object {
-        private val sentinel = Node(null, null)
-    }
-
-    // Invariant: once head stores null, it will always store null.
-    // Head storing null indicates the end of the stack usage. The stack is considered closed.
-    // Any further push will return false. Any further drain will drain an empty list.
-    private val head = atomic<Node?>(sentinel)
-
-    fun push(element: ParkingHandle): Boolean {
-        val node = Node(element, null)
-        head.loop { cur ->
-            if (cur == null) return false
-            node.next = cur
-            if (head.compareAndSet(cur, node)) return true
+    fun enqueue(element: E) {
+        while (true) {
+            val node = Node(element)
+            val curTail = tail.value
+            if (curTail.next.compareAndSet(null, node)) {
+                tail.compareAndSet(curTail, node)
+                return
+            } else tail.compareAndSet(curTail, curTail.next.value!!)
         }
     }
 
-    fun drain(consumer: (ParkingHandle) -> Unit) {
-        var node = head.getAndSet(null)
-        while (node != null) {
-            if (node == sentinel) break
-            consumer(node.data!!)
-            node = node.next
+    fun dequeue(): E? {
+        while (true) {
+            val currentHead = head.value
+            val currentHeadNext = currentHead.next.value ?: return null
+            if (head.compareAndSet(currentHead, currentHeadNext)) {
+                val element = currentHeadNext.element
+                currentHeadNext.element = null
+                return element
+            }
         }
+    }
+
+    private class Node<E>(var element: E?) {
+        val next = atomic<Node<E>?>(null)
     }
 }
