eb29ef81017294b36019a8671cc3a07556ceef63
Margarita Bobova <margarita.bobova@jetbrains.com>
2026-02-23 18:50:02 +0100

    integration-testing: add kotlin_repo_url for train

--- integration-testing/build.gradle.kts
+++ integration-testing/build.gradle.kts
@@ -39,6 +39,7 @@ buildscript {
 
     val usingSnapshotVersion = checkIsSnapshotVersion()
     val hasSnapshotTrainProperty = checkIsSnapshotTrainProperty()
+    val kotlinRepoUrl = providers.gradleProperty("kotlin_repo_url").orNull
 
     extra.apply {
         set("using_snapshot_version", usingSnapshotVersion)
@@ -50,6 +51,9 @@ buildscript {
             mavenLocal()
             maven("https://redirector.kotlinlang.org/maven/dev")
             maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+            if (!kotlinRepoUrl.isNullOrBlank()) {
+                maven(kotlinRepoUrl)
+            }
         }
     }
 }
@@ -63,9 +67,31 @@ repositories {
         maven("https://redirector.kotlinlang.org/maven/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
+    val kotlinRepoUrl = providers.gradleProperty("kotlin_repo_url").orNull
+    if (!kotlinRepoUrl.isNullOrBlank()) {
+        maven(kotlinRepoUrl)
+    }
     mavenLocal()
     maven("https://redirector.kotlinlang.org/maven/dev")
     mavenCentral()
+}
+
+subprojects {
+    repositories {
+        val kotlinRepoUrl = providers.gradleProperty("kotlin_repo_url").orNull
+        val buildSnapshotTrain = providers.gradleProperty("build_snapshot_train").orNull?.toBoolean() == true
+        val includeKotlinDevRepos = buildSnapshotTrain || name != "jpmsTest"
+
+        if (!kotlinRepoUrl.isNullOrBlank()) {
+            maven(kotlinRepoUrl)
+        }
+        mavenCentral()
+        if (includeKotlinDevRepos) {
+            maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+            maven("https://redirector.kotlinlang.org/maven/dev")
+        }
+        mavenLocal()
+    }
 }
 
 java {
--- integration-testing/settings.gradle.kts
+++ integration-testing/settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
         maven("https://plugins.gradle.org/m2/")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         maven("https://redirector.kotlinlang.org/maven/dev")
+        val kotlinRepoUrl = providers.gradleProperty("kotlin_repo_url").orNull
+        if (!kotlinRepoUrl.isNullOrBlank()) {
+            maven(kotlinRepoUrl)
+        }
         mavenLocal()
     }
 }
