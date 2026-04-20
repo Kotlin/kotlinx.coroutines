2e550d131a447cae09c251c0979e0ee046a92e0a
Natasha Murashkina <murfel@users.noreply.github.com>
2026-04-20 10:31:44 +0100

    Revert "Ensure that inline functions only access atomics from the same class (#4041)"
    This reverts commit 29e821302f2b18b75252489997cc22d4a3fbdc09.
    
    # Conflicts:
    #	kotlinx-coroutines-core/common/src/channels/BufferedChannel.kt

--- kotlinx-coroutines-core/common/src/channels/ConflatedBufferedChannel.kt
+++ kotlinx-coroutines-core/common/src/channels/ConflatedBufferedChannel.kt
@@ -1,6 +1,7 @@
 package kotlinx.coroutines.channels
 
 import kotlinx.coroutines.channels.BufferOverflow.*
+import kotlinx.coroutines.channels.ChannelResult.Companion.closed
 import kotlinx.coroutines.channels.ChannelResult.Companion.success
 import kotlinx.coroutines.internal.*
 import kotlinx.coroutines.selects.*
@@ -67,6 +68,27 @@ internal open class ConflatedBufferedChannel<E>(
         }
         return success(Unit)
     }
+
+    private fun trySendDropOldest(element: E): ChannelResult<Unit> =
+        sendImpl( // <-- this is an inline function
+            element = element,
+            // Put the element into the logical buffer even
+            // if this channel is already full, the `onSuspend`
+            // callback below extract the first (oldest) element.
+            waiter = BUFFERED,
+            // Finish successfully when a rendezvous has happened
+            // or the element has been buffered.
+            onRendezvousOrBuffered = { return success(Unit) },
+            // In case the algorithm decided to suspend, the element
+            // was added to the buffer. However, as the buffer is now
+            // overflowed, the first (oldest) element has to be extracted.
+            onSuspend = { segm, i ->
+                dropFirstElementUntilTheSpecifiedCellIsInTheBuffer(segm.id * SEGMENT_SIZE + i)
+                return success(Unit)
+            },
+            // If the channel is closed, return the corresponding result.
+            onClosed = { return closed(sendException) }
+        )
 
     @Suppress("UNCHECKED_CAST")
     override fun registerSelectForSend(select: SelectInstance<*>, element: Any?) {
