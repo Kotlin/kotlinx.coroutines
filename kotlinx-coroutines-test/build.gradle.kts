f17e8bace46b7b88de97771c257d82a390977668
Dmitry Khalanskiy <52952525+dkhalanskyjb@users.noreply.github.com>
2026-07-07 13:24:27 +0200

    Simplify the build scripts (#4610)

--- kotlinx-coroutines-test/build.gradle.kts
+++ kotlinx-coroutines-test/build.gradle.kts
@@ -1,22 +1,8 @@
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-
 kotlin {
     sourceSets {
         jvmTest {
             dependencies {
                 implementation(project(":kotlinx-coroutines-debug"))
-            }
-        }
-    }
-
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        nodejs {
-            testTask {
-                filter.apply {
-                    // https://youtrack.jetbrains.com/issue/KT-61888
-                    excludeTest("TestDispatchersTest", "testMainMocking")
-                }
             }
         }
     }
