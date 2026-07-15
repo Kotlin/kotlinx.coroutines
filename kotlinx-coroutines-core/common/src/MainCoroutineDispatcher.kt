cca9fc3fae621416536741b0307b7394a7691ca8
Dmitry Khalanskiy <dmitry.khalanskiy@jetbrains.com>
2026-07-15 16:37:25 +0200

    Do not fail in MainCoroutineDispatcher.toString when .immediate fails

--- kotlinx-coroutines-core/common/src/MainCoroutineDispatcher.kt
+++ kotlinx-coroutines-core/common/src/MainCoroutineDispatcher.kt
@@ -68,10 +68,11 @@ public abstract class MainCoroutineDispatcher : CoroutineDispatcher() {
             return null
         }
         if (this === main) return "Dispatchers.Main"
-        val immediate =
-            try { main.immediate }
-            catch (e: UnsupportedOperationException) { null }
-        if (this === immediate) return "Dispatchers.Main.immediate"
-        return null
+        val immediate = try {
+            main.immediate
+        } catch (_: Throwable) {
+            return null
+        }
+        return if (this === immediate) "Dispatchers.Main.immediate" else null
     }
 }
