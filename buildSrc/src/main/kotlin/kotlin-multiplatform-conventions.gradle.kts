a237873d933164539f8641bf5d6abd1bc4e66d60
Artem Olkov <artem.olkov@jetbrains.com>
2026-07-16 12:06:31 +0200

    KT-78078: remove watchosArm32

--- buildSrc/src/main/kotlin/kotlin-multiplatform-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-multiplatform-conventions.gradle.kts
@@ -36,7 +36,6 @@ kotlin {
         // Tier 2
         linuxArm64()
         watchosSimulatorArm64()
-        watchosArm32()
         watchosArm64()
         tvosSimulatorArm64()
         tvosArm64()
