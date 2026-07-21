63f02c069ceab3db174273a5857a328fcdbd059a
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2026-07-21 14:45:21 +0200

    Remove extraneous atomics

--- kotlinx-coroutines-core/concurrent/test/flow/StateFlowCancellabilityTest.kt
+++ kotlinx-coroutines-core/concurrent/test/flow/StateFlowCancellabilityTest.kt
@@ -12,12 +12,12 @@ class StateFlowCancellabilityTest : TestBase() {
     fun testCancellabilityNoConflation() = runTest {
         expect(1)
         val state = MutableStateFlow(0)
-        val subscribed = AtomicBoolean(false)
-        val lastReceived = AtomicInt(-1)
+        var subscribed = false
+        var lastReceived = -1
         val barrier = TwoPhaseBarrier(2)
         val job = state
             .onSubscription {
-                subscribed.store(true)
+                subscribed = true
                 barrier.await() // 1
             }
             .onEach { i ->
@@ -30,21 +30,21 @@ class StateFlowCancellabilityTest : TestBase() {
                     }
                     else -> expectUnreached() // shall check for cancellation
                 }
-                lastReceived.store(i)
+                lastReceived = i
                 barrier.await() // 2
                 barrier.await() // 3
             }
             .launchIn(this + Dispatchers.Default)
         barrier.await() // 1
-        assertTrue(subscribed.load()) // should have subscribed in the first barrier
+        assertTrue(subscribed) // should have subscribed in the first barrier
         barrier.await() // 2
-        assertEquals(0, lastReceived.load()) // should get initial value, too
+        assertEquals(0, lastReceived) // should get initial value, too
         for (i in 1..3) { // emit after subscription
             state.value = i
             barrier.await() // 3, let it go
             if (i < 3) {
                 barrier.await() // 2, wait for receive
-                assertEquals(i, lastReceived.load()) // shall receive it
+                assertEquals(i, lastReceived) // shall receive it
             }
         }
         job.join()
