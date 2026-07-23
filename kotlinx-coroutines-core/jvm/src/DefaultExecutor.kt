ff5e5e909223ee1609104d735105e34a1484acf3
Max Faber <max@maxfaber.dev>
2026-07-23 11:22:30 +0200

    Fix typo in comment in DefaultExecutor (#4708)

--- kotlinx-coroutines-core/jvm/src/DefaultExecutor.kt
+++ kotlinx-coroutines-core/jvm/src/DefaultExecutor.kt
@@ -184,7 +184,7 @@ internal actual object DefaultExecutor : EventLoopImplBase(), Runnable {
         (this as Object).notifyAll()
     }
 
-    // User only for testing and nothing else
+    // Used only for testing and nothing else
     internal val isThreadPresent
         get() = _thread != null
 
