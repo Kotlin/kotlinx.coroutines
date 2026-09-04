81c95731e6801e077dabbd85336298d59da54c19
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-09-04 16:00:20 -0400

    Fix SMARTCAST_IMPOSSIBLE error

--- reactive/kotlinx-coroutines-rx3/test/ObservableAsFlowTest.kt
+++ reactive/kotlinx-coroutines-rx3/test/ObservableAsFlowTest.kt
@@ -128,7 +128,7 @@ class ObservableAsFlowTest : TestBase() {
         assertNotNull(observer)
         job.cancel()
         val disposable = Disposable.empty()
-        observer.onSubscribe(disposable)
+        observer!!.onSubscribe(disposable)
         assertTrue(disposable.isDisposed)
         finish(3)
     }
